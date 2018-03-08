4636a3ea632001b3cbb44dec6dadbcddddf39bbf
zhanglongfei <zhanglongfei@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-08 02:22:37 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@50293 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

---
+++ src/main/java/cn/financial/model/Organization.java
@@ -0,0 +1,70 @@
+package cn.financial.model;
+
+import java.util.Date;
+
+public class Organization {
+    
+         private String id;   //组织架构表id
+         
+         private String parentId; //父id
+         
+         private String orgName;  //组织架构名
+         
+         private Date createTime; //创建时间
+         
+         private Date updateTime; //更新时间
+         
+         private String uId;        //提交人id
+         
+         
+    
+        public String getId() {
+            return id;
+        }
+    
+        public void setId(String id) {
+            this.id = id;
+        }
+    
+        public String getParentId() {
+            return parentId;
+        }
+    
+        public void setParentId(String parentId) {
+            this.parentId = parentId;
+        }
+    
+        public String getOrgName() {
+            return orgName;
+        }
+    
+        public void setOrgName(String orgName) {
+            this.orgName = orgName;
+        }
+    
+        public Date getCreateTime() {
+            return createTime;
+        }
+    
+        public void setCreateTime(Date createTime) {
+            this.createTime = createTime;
+        }
+    
+        public Date getUpdateTime() {
+            return updateTime;
+        }
+    
+        public void setUpdateTime(Date updateTime) {
+            this.updateTime = updateTime;
+        }
+    
+        public String getuId() {
+            return uId;
+        }
+    
+        public void setuId(String uId) {
+            this.uId = uId;
+        }
+    
+           
+}
