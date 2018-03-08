040934f7444022374d1e58404ebdf2877b108f59
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-08 08:50:13 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@50340 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/dao/UserDAO.java
+++ src/main/java/cn/financial/dao/UserDAO.java
@@ -1,13 +1,53 @@
 package cn.financial.dao;
 
 import java.awt.List;
+import java.util.Date;
 
 import cn.financial.model.User;
 
-
+/**
+ * 
+ * @author gs
+ * 2018/3/7
+ */
 public interface UserDAO {
-    public int insertUser(User user);
-    
-    public User selectUserById(Integer userId);
- 
+    /**
+     * 查询所有用户
+     * @return
+     */
+    java.util.List<User> listUser();
+    /**
+     * 根据name查询用户
+     * @param name
+     * @return
+     */
+    int listUserById(String name);
+    /**
+     * 新增用户
+     * @param name
+     * @param pwd
+     * @param privilege
+     * @param createTime
+     * @param updateTime
+     * @param oId
+     * @return
+     */
+    int insertUser(User user);
+    /**
+     * 修改用户
+     * @param name
+     * @param pwd
+     * @param privilege
+     * @param createTime
+     * @param updateTime
+     * @param oId
+     * @return
+     */
+    int updateUser(User user);
+    /**
+     * 删除用户
+     * @param userId
+     * @return
+     */
+    int deleteUser(String userId);
 }
