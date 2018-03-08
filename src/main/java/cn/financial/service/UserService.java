c3e4eaa2b6a5170ed765a57e54690b2c1c7302c4
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-08 08:50:19 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@50341 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/service/UserService.java
+++ src/main/java/cn/financial/service/UserService.java
@@ -1,12 +1,51 @@
 package cn.financial.service;
 
 import java.awt.List;
+import java.util.Date;
 
+import cn.financial.model.Organization;
 import cn.financial.model.User;
 
 
 public interface UserService {
-	 public int insertUser(User user);
-	 public User selectUserById(Integer userId);
+    /**
+     * 查询所有用户
+     * @return
+     */
+    java.util.List<User> listUser();
+    /**
+     * 根据name查询用户
+     * @param name
+     * @return
+     */
+    int listUserById(String name);
+    /**
+     * 新增用户
+     * @param name
+     * @param pwd
+     * @param privilege
+     * @param createTime
+     * @param updateTime
+     * @param oId
+     * @return
+     */
+    int insertUser(String name, String pwd, Integer privilege, Date createTime, Date updateTime, String oId);
+    /**
+     * 修改用户
+     * @param name
+     * @param pwd
+     * @param privilege
+     * @param createTime
+     * @param updateTime
+     * @param oId
+     * @return
+     */
+    int updateUser(String userId, String name, String pwd, Integer privilege, Date createTime, Date updateTime, String oId);
+    /**
+     * 删除用户
+     * @param userId
+     * @return
+     */
+    int deleteUser(String userId);
 	 
 }
