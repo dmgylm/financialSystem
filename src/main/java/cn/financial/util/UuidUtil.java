dc17cb3c7335eb554534438a6dd6e994d7c51224
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-08 08:50:28 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@50343 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/util/UuidUtil.java
+++ src/main/java/cn/financial/util/UuidUtil.java
@@ -1,6 +1,11 @@
 package cn.financial.util;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
+
+import cn.financial.model.Organization;
+import cn.financial.model.User;
 
 /**
  * UUID 工具类
@@ -20,5 +25,20 @@ public class UuidUtil {
         String str = uuid.toString();
         String uuidStr = str.replace("-", "");
         return uuidStr;
+    }
+    
+    /**
+     * user
+     */
+    public static User strToUser(String userId, String name, String pwd, Integer privilege, Date createTime, Date updateTime, String oId){
+        User user = new User();
+        user.setId(userId);
+        user.setName(name);
+        user.setPwd(pwd);
+        user.setPrivilege(privilege);
+        user.setCreateTime(createTime);
+        user.setUpdateTime(updateTime);
+        user.setoId(oId);
+        return user;
     }
 }
