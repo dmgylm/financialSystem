39e982a3cc6b92e9354cf78141159a2b1b98e975
zhanglongfei <zhanglongfei@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-08 02:35:36 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@50295 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/model/Organization.java
+++ src/main/java/cn/financial/model/Organization.java
@@ -1,70 +1,68 @@
 package cn.financial.model;
 
 import java.util.Date;
+import java.util.List;
 
 public class Organization {
-    
-         private String id;   //组织架构表id
-         
-         private String parentId; //父id
-         
-         private String orgName;  //组织架构名
-         
-         private Date createTime; //创建时间
-         
-         private Date updateTime; //更新时间
-         
-         private String uId;        //提交人id
-         
-         
-    
-        public String getId() {
-            return id;
-        }
-    
-        public void setId(String id) {
-            this.id = id;
-        }
-    
-        public String getParentId() {
-            return parentId;
-        }
-    
-        public void setParentId(String parentId) {
-            this.parentId = parentId;
-        }
-    
-        public String getOrgName() {
-            return orgName;
-        }
-    
-        public void setOrgName(String orgName) {
-            this.orgName = orgName;
-        }
-    
-        public Date getCreateTime() {
-            return createTime;
-        }
-    
-        public void setCreateTime(Date createTime) {
-            this.createTime = createTime;
-        }
-    
-        public Date getUpdateTime() {
-            return updateTime;
-        }
-    
-        public void setUpdateTime(Date updateTime) {
-            this.updateTime = updateTime;
-        }
-    
-        public String getuId() {
-            return uId;
-        }
-    
-        public void setuId(String uId) {
-            this.uId = uId;
-        }
-    
-           
+
+    private String id; // 组织架构表id
+
+    private String parentId; // 父id
+
+    private String orgName; // 组织架构名
+
+    private Date createTime; // 创建时间
+
+    private Date updateTime; // 更新时间
+
+    private List<User> users; // 提交人id（一对多，组织结构为一）
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
 }
