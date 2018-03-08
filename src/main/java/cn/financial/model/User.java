506c04bcbfda19bdc294c4fc0f3ef111976a8ad3
zhanglongfei <zhanglongfei@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-08 02:43:55 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@50296 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/model/User.java
+++ src/main/java/cn/financial/model/User.java
@@ -3,74 +3,75 @@ package cn.financial.model;
 import java.util.Date;
 
 public class User {
-	 
-    	 private String id;  //用户表id
-    	 
-    	 private String name; // 用户名
-    	 
-    	 private String pwd;  //密码
-    	 
-    	 private int privilege;  //权限级别
-    	 
-         private Date createTime; //创建时间
-    	 
-    	 private Date updateTime; //更新时间
-    	 
-    	 private Organization oId; //组织id
-    
-    	public String getId() {
-    		return id;
-    	}
-    
-    	public void setId(String id) {
-    		this.id = id;
-    	}
-    
-    	public String getName() {
-    		return name;
-    	}
-    
-    	public void setName(String name) {
-    		this.name = name;
-    	}
-    
-    	public String getPwd() {
-    		return pwd;
-    	}
-    
-    	public void setPwd(String pwd) {
-    		this.pwd = pwd;
-    	}
-    
-    	public int getPrivilege() {
-    		return privilege;
-    	}
-    
-    	public void setPrivilege(int privilege) {
-    		this.privilege = privilege;
-    	}
-    
-    	public Date getCreateTime() {
-    		return createTime;
-    	}
-    
-    	public void setCreateTime(Date createTime) {
-    		this.createTime = createTime;
-    	}
-    
-    	public Date getUpdateTime() {
-    		return updateTime;
-    	}
-    
-    	public void setUpdateTime(Date updateTime) {
-    		this.updateTime = updateTime;
-    	}
 
-        public Organization getoId() {
-            return oId;
-        }
+    private String id; // 用户表id
 
-        public void setoId(Organization oId) {
-            this.oId = oId;
-        }
+    private String name; // 用户名
+
+    private String pwd; // 密码
+
+    private int privilege; // 权限级别
+
+    private Date createTime; // 创建时间
+
+    private Date updateTime; // 更新时间
+
+    private Organization oId; // 组织id
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public int getPrivilege() {
+        return privilege;
+    }
+
+    public void setPrivilege(int privilege) {
+        this.privilege = privilege;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Organization getoId() {
+        return oId;
+    }
+
+    public void setoId(Organization oId) {
+        this.oId = oId;
+    }
+
 }
