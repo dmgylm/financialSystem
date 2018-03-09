66f19025625745ca68545c4e3013a7a759012d32
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-09 02:28:21 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@50386 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/test/userTest.java
+++ src/main/java/test/userTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import cn.financial.model.User;
-import cn.financial.service.UserService;
 import cn.financial.service.impl.UserServiceImpl;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:conf/spring-test.xml")
@@ -23,26 +22,27 @@ import cn.financial.service.impl.UserServiceImpl;
 public class userTest {
     @Autowired
     private UserServiceImpl service;
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
     //新增
     @Test
-    void insertTest() {
+    public void insertTest() {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         String cTime="2018-03-05";
         String uTime="2018-03-07";
         try {
-            System.out.println(service.insertUser("a", "b", 2, formatter.parse(cTime), formatter.parse(uTime), "3"));
+            System.out.println(service.insertUser("雯雯", "小芳", "b", 2, formatter.parse(cTime), formatter.parse(uTime), "3"));
         } catch (ParseException e) {
             System.out.println(e.getMessage());
         }
     }
     //修改
     @Test
-    void updateTest() {
+    public void updateTest() {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         String cTime="2018-03-06";
         String uTime="2018-03-08";
         try {
-            System.out.println(service.updateUser("1ccb0f2ca6224f389cbbea57b85d4458", "aa11", "13233", 2, formatter.parse(cTime), formatter.parse(uTime), "4"));
+            System.out.println(service.updateUser("72dee616075f4a42acf444fe729161a6", "222", "小莉", "", 2, formatter.parse(cTime), formatter.parse(uTime), ""));
         } catch (ParseException e) {
             System.out.println(e.getMessage());
         }
@@ -50,23 +50,29 @@ public class userTest {
     //删除
     @Test
     public void deleteTest() {   
-        System.out.println(service.deleteUser("1"));
+        System.out.println(service.deleteUser("33333"));
     }
     //查询全部
     @Test
     public void ListUserTest() {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         List<User> user = service.listUser();
         for(User list:user){
-            System.out.println("id: "+list.getId() +"name: "+list.getName() +"pwd: "+list.getPwd() +"privilege: "+list.getPrivilege() +
-                    "createTime: "+formatter.format(list.getCreateTime()) +"updateTime: "+formatter.format(list.getUpdateTime()) +"oId: "+list.getoId()+"\n"); 
+            System.out.println("id: "+list.getId() +" name: "+list.getName() +" pwd: "+list.getPwd() +" privilege: "+list.getPrivilege() +
+                    " createTime: "+formatter.format(list.getCreateTime()) +" updateTime: "+formatter.format(list.getUpdateTime()) +" oId: "+list.getoId()+"\n"); 
         }
         
     }
     //根据name查询
     @Test
     public void ListNameTest() {
-        int flag = service.listUserById("a");
+        int flag = service.countUserName("a");
         System.out.println(flag);
+    }
+    //根据id查询
+    @Test
+    public void ListByIdTest() {
+        User user = service.getUserById("1ccb0f2ca6224f389cbbea57b85d4458");
+        System.out.println("id: "+user.getId()+" name: "+user.getName()+" realName: "+user.getRealName()+" pwd: "+user.getPwd()+" privilege: "+user.getPrivilege()+
+                " createTime: "+formatter.format(user.getCreateTime())+" updateTime: "+formatter.format(user.getUpdateTime())+" oId: "+user.getoId());
     }
 }
