3df803e9c6b2663d6a17f48f792899a5e6a6ddc9
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-09 02:42:56 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@50396 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/service/impl/UserServiceImpl.java
+++ src/main/java/cn/financial/service/impl/UserServiceImpl.java
@@ -36,13 +36,15 @@ public class UserServiceImpl implements  UserService{
      * 新增用户
      */
     public int insertUser(String name, String realName, String pwd, Integer privilege, Date createTime, Date updateTime, String oId) {
-        return userDao.insertUser(UuidUtil.strToUser(UuidUtil.getUUID(), name, realName, pwd, privilege, createTime, updateTime, oId));
+        User user = new User(UuidUtil.getUUID(), name, realName, pwd, privilege, createTime, updateTime, oId);
+        return userDao.insertUser(user);
     }
     /**
      * 修改用户
      */
     public int updateUser(String userId, String name, String realName, String pwd, Integer privilege, Date createTime, Date updateTime, String oId) {
-        return userDao.updateUser(UuidUtil.strToUser(userId, name, realName, pwd, privilege, createTime, updateTime, oId));
+        User user = new User(userId, name, realName, pwd, privilege, createTime, updateTime, oId);
+        return userDao.updateUser(user);
     }
     /**
      * 删除用户
