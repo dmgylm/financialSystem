9c61cd45e3ea1ec45000388bba7dd58e2deae2f0
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-09 02:28:12 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@50384 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/service/impl/UserServiceImpl.java
+++ src/main/java/cn/financial/service/impl/UserServiceImpl.java
@@ -1,13 +1,10 @@
 package cn.financial.service.impl;
-
-import java.awt.List;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.financial.dao.UserDAO;
-import cn.financial.model.Organization;
 import cn.financial.model.User;
 import cn.financial.service.UserService;
 import cn.financial.util.UuidUtil;
@@ -26,20 +23,26 @@ public class UserServiceImpl implements  UserService{
     /**
      * 根据name查询
      */
-    public int listUserById(String name) {
-        return userDao.listUserById(name);
+    public int countUserName(String name) {
+        return userDao.countUserName(name);
+    }
+    /**
+     * 根据id查询
+     */
+    public User getUserById(String userId){
+        return userDao.getUserById(userId);
     }
     /**
      * 新增用户
      */
-    public int insertUser(String name, String pwd, Integer privilege, Date createTime, Date updateTime, String oId) {
-        return userDao.insertUser(UuidUtil.strToUser(UuidUtil.getUUID(),name, pwd, privilege, createTime, updateTime, oId));
+    public int insertUser(String name, String realName, String pwd, Integer privilege, Date createTime, Date updateTime, String oId) {
+        return userDao.insertUser(UuidUtil.strToUser(UuidUtil.getUUID(), name, realName, pwd, privilege, createTime, updateTime, oId));
     }
     /**
      * 修改用户
      */
-    public int updateUser(String userId, String name, String pwd, Integer privilege, Date createTime, Date updateTime, String oId) {
-        return userDao.updateUser(UuidUtil.strToUser(userId,name, pwd, privilege, createTime, updateTime, oId));
+    public int updateUser(String userId, String name, String realName, String pwd, Integer privilege, Date createTime, Date updateTime, String oId) {
+        return userDao.updateUser(UuidUtil.strToUser(userId, name, realName, pwd, privilege, createTime, updateTime, oId));
     }
     /**
      * 删除用户
