cc58adb1e571880e54113af5a2976ce290f80784
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-13 06:46:09 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@50699 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/test/java/cn/springmvc/test/UserTest.java
+++ src/test/java/cn/springmvc/test/UserTest.java
@@ -1,7 +1,7 @@
 package cn.springmvc.test;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import cn.financial.model.User;
 import cn.financial.service.impl.UserServiceImpl;
+import cn.financial.util.UuidUtil;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:conf/spring.xml", "classpath:conf/spring-mvc.xml",
         "classpath:conf/spring-mybatis.xml", "classpath:conf/mybatis-config.xml" })
@@ -27,31 +28,42 @@ public class UserTest {
     //新增
     @Test
     public void insertTest() {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        String cTime="2018-03-05";
-        String uTime="2018-03-07";
+        User user = new User();
+        user.setId(UuidUtil.getUUID());
+        user.setName("呜呜呜呜");
+        user.setRealName("凄凄切切");
+        user.setPwd("555666");
+        user.setJobNumber("743211124420888");
+        user.setCreateTime(new Date());
+        user.setUpdateTime(new Date());
+        user.setoId("555");
         try {
-            System.out.println(service.insertUser("eee", "ccc", "b", 2, formatter.parse(cTime), formatter.parse(uTime), "355"));
-        } catch (ParseException e) {
+            System.out.println(service.insertUser(user));
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
     //修改
     @Test
     public void updateTest() {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        String cTime="2018-03-06";
-        String uTime="2018-03-08";
+        User user = new User();
+        user.setId("2");
+        user.setName("听听");
+        user.setRealName("拖拖拖");
+        user.setPwd("555666");
+        user.setJobNumber("7432111244208");
+        user.setCreateTime(new Date());
+        user.setUpdateTime(new Date());
+        user.setoId("555");
         try {
-            System.out.println(service.updateUser("a6c920ed8c8a4d149869d19088945205", "1", "55", "", 2, formatter.parse(cTime), formatter.parse(uTime), ""));
-        } catch (ParseException e) {
+            System.out.println(service.updateUser(user));
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
     //删除
     @Test
     public void deleteTest() {   
-        //System.out.println(service.deleteUser("33333"));
         System.out.println(service.deleteUser("2"));
     }
     //查询全部
@@ -59,7 +71,7 @@ public class UserTest {
     public void ListUserTest() {
         List<User> user = service.listUser();
         for(User list:user){
-            System.out.println("id: "+list.getId() +" name: "+list.getName() +" pwd: "+list.getPwd() +" privilege: "+list.getPrivilege() +
+            System.out.println("id: "+list.getId() +" name: "+list.getName() +" pwd: "+list.getPwd() +
                     " createTime: "+formatter.format(list.getCreateTime()) +" updateTime: "+formatter.format(list.getUpdateTime()) +" oId: "+list.getoId()+"\n"); 
         }
         
@@ -67,14 +79,14 @@ public class UserTest {
     //根据name,pwd查询
     @Test
     public void ListNameTest() {
-        Integer flag = service.countUserName("李四","444");
+        Integer flag = service.countUserName("哈哈好","555666");
         System.out.println(flag);
     }
     //根据id查询
     @Test
     public void ListByIdTest() {
         User user = service.getUserById("1ccb0f2ca6224f389cbbea57b85d4458");
-        System.out.println("id: "+user.getId()+" name: "+user.getName()+" realName: "+user.getRealName()+" pwd: "+user.getPwd()+" privilege: "+user.getPrivilege()+
+        System.out.println("id: "+user.getId()+" name: "+user.getName()+" realName: "+user.getRealName()+" pwd: "+user.getPwd()+
                 " createTime: "+formatter.format(user.getCreateTime())+" updateTime: "+formatter.format(user.getUpdateTime())+" oId: "+user.getoId());
     }
 }
