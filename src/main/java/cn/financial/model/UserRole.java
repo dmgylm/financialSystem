1a63e0cecdf814523674a178ba528132fa410344
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-14 06:51:46 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@50787 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/model/UserRole.java
+++ src/main/java/cn/financial/model/UserRole.java
@@ -3,53 +3,82 @@ package cn.financial.model;
 import java.util.Date;
 
 public class UserRole {
+    
     private String id;//用户角色关联表id
     private String rId;//角色id
     private String uId;//用户id
-    private Date createTime;//创建时间
-    private Date updateTime;//修改时间
+    private String roleName;//角色名称
+    private String realName;//真实姓名
+    private String name;//姓名
+    private String jobNumber;//工号
+    private Date createTime; //创建时间
     
     public UserRole() {
         super();
     }
-    
-    public UserRole(String id, String rId, String uId, Date createTime, Date updateTime) {
-        super();
-        this.id = id;
-        this.rId = rId;
-        this.uId = uId;
-        this.createTime = createTime;
-        this.updateTime = updateTime;
-    }
+
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public String getrId() {
         return rId;
     }
+
     public void setrId(String rId) {
         this.rId = rId;
     }
+
     public String getuId() {
         return uId;
     }
+
     public void setuId(String uId) {
         this.uId = uId;
     }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJobNumber() {
+        return jobNumber;
+    }
+
+    public void setJobNumber(String jobNumber) {
+        this.jobNumber = jobNumber;
+    }
+
     public Date getCreateTime() {
         return createTime;
     }
+
     public void setCreateTime(Date createTime) {
         this.createTime = createTime;
     }
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-}
+    
+ }
