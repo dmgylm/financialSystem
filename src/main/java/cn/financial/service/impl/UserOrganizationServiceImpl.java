4e0cdba3e24ad0590693bde3a1a774b9db0055e9
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-16 08:04:00 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@51041 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

---
+++ src/main/java/cn/financial/service/impl/UserOrganizationServiceImpl.java
@@ -0,0 +1,35 @@
+package cn.financial.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.financial.dao.UserOrganizationDAO;
+import cn.financial.model.UserOrganization;
+import cn.financial.service.UserOrganizationService;
+
+
+@Service("UserOrganizationServiceImpl")
+public class UserOrganizationServiceImpl implements UserOrganizationService{
+    @Autowired
+    private UserOrganizationDAO userOrganizationDao;
+    /**
+     * 查询所有
+     * @return
+     */
+    @Override
+    public List<UserOrganization> listUserOrganization() {
+        return userOrganizationDao.listUserOrganization();
+    }
+    /**
+     * 新增
+     * @return
+     */
+    @Override
+    public Integer insertUserOrganization(UserOrganization userOrganization) {
+        return userOrganizationDao.insertUserOrganization(userOrganization);
+    }
+
+}
+ 
