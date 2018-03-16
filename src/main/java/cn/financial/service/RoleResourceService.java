b5aaebedd29b36da995da02fb19ad0d8f33c3b73
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-16 03:15:09 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@50979 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/service/RoleResourceService.java
+++ src/main/java/cn/financial/service/RoleResourceService.java
@@ -7,10 +7,10 @@ import cn.financial.model.RoleResource;
 
 public interface RoleResourceService {
     /**
-     * 查询所有
+     * 查询所有/根据角色查对应的功能权限
      * @return
      */
-    List<RoleResource> listRoleResource();
+    List<RoleResource> listRoleResource(String roleName);
     /**
      * 新增
      * @param roleResource
