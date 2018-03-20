303ece75ac13794e5c0f7f4445c6ba8a1b996741
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-20 03:07:48 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@51206 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/controller/MainController.java
+++ src/main/java/cn/financial/controller/MainController.java
@@ -3,13 +3,24 @@ package cn.financial.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.ExcessiveAttemptsException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
+
+
 /**
  * 控制页面跳转
- * @author rzm
+ * @author gs
  *
  */
 @Controller
@@ -20,11 +31,34 @@ public class MainController {
      * @param respons
      * @return
      */
-    @RequestMapping(value="/manage/login")
-    public ModelAndView login(HttpServletRequest request,HttpServletResponse respons){
-        ModelAndView andView=new ModelAndView();
-        andView.setViewName("manage/login");
-        return andView;
+    @RequestMapping(value="/login")
+    public String login(HttpServletRequest request,HttpServletResponse respons ,Model model){
+        //String exceptionClassName = (String)request.getAttribute("shiroLoginFailure");
+        String error = null;
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(request.getParameter("name"),request.getParameter("pwd"));
+        try {
+            subject.login(token);
+        } catch (UnknownAccountException e) {
+            System.out.println( "用户名/密码错误");
+            error = "用户名/密码错误";  
+        } catch (IncorrectCredentialsException e) { 
+            System.out.println( "用户名/密码错误");
+            error = "用户名/密码错误";  
+        } catch (ExcessiveAttemptsException e) {  
+            System.out.println("登录失败多次，账户锁定10分钟");
+            error = "登录失败多次，账户锁定10分钟";  
+        } catch (AuthenticationException e) {  
+            System.out.println( "其他错误：" + e.getMessage());
+            // 其他错误，比如锁定，如果想单独处理请单独catch处理  
+            error = "其他错误：" + e.getMessage();  
+        } 
+        if (error != null) {// 出错了，返回登录页面  
+            request.setAttribute("error", error);  
+            return "/login";  
+        } else {// 登录成功  
+            return "/index";  
+        } 
     }
     /**
      * index
@@ -32,10 +66,10 @@ public class MainController {
      * @param respons
      * @return
      */
-	@RequestMapping(value="/manage/index")
+	@RequestMapping(value="/index")
     public ModelAndView index(HttpServletRequest request,HttpServletResponse respons){
     	ModelAndView andView=new ModelAndView();
-    	andView.setViewName("manage/index");
+    	andView.setViewName("index");
     	return andView;
     }
 }
