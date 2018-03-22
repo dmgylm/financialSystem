bea10682631708570d6958d5e8a02008b65878ec
chengsen <chengsen@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-03-22 06:53:19 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@51540 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/test/java/cn/springmvc/test/MessageTest.java
+++ src/test/java/cn/springmvc/test/MessageTest.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,41 +17,54 @@ import cn.financial.util.UuidUtil;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:conf/spring.xml", "classpath:conf/spring-mvc.xml",
-        "classpath:conf/spring-mybatis.xml", "classpath:conf/mybatis-config.xml" })
+        "classpath:conf/spring-mybatis.xml", "classpath:conf/mybatis-config.xml", "classpath:conf/spring-cache.xml",
+        "classpath:conf/spring-shiro.xml" })
 public class MessageTest {
 
     @Autowired
     private MessageServiceImpl service;
-
+    
     /**
      * 新增消息表
      */
-    @Test
+    //@Test
     public void saveMessage() {
+    	PropertyConfigurator.configure(MessageTest.class.getClassLoader().getResource("conf/log4j.properties"));
+
         Message message = new Message();
         message.setId(UuidUtil.getUUID());
-        message.setStatus(1);
+        message.setStatus(0);
         message.setTheme(1);
         message.setContent("2018年维修表格需要填写");
-        message.setuId("1ccb0f2ca6224f389cbbea57b85d4458");
+        message.setuId("1d9fa5e93ffe46d78bb351ac05e70420");
+        message.setsuId("rr");
         Integer i = service.saveMessage(message);
         System.out.println(i);
     }
 
     /**
-     * 查询所有的消息表
+     * 根据消息状态展示消息列表
      */
     @Test
     public void listMessage() {
-        List<Message> list = service.listMessage();
+    	Map<Object, Object> map = new HashMap<Object, Object>();
+    	
+    	map.put("uId","1d9fa5e93ffe46d78bb351ac05e70420" );
+    	map.put("isTag", 0);
+        List<Message> list = service.listMessage(map);
+        int wstatus=0;
+        for(int i=0;i<list.size();i++) {
+        	if(list.get(i).getStatus()==0) {
+        		wstatus+=1;
+        	}
+        }
+        System.out.println(wstatus);
         for (Message message : list) {
-            System.out.println("id:" + message.getId());
             System.out.println("status:" + message.getStatus());
             System.out.println("theme:" + message.getTheme());
             System.out.println("content:" + message.getContent());
+            System.out.println("suId:" + message.getsuId());
             System.out.println("createTime:" + message.getCreateTime());
-            System.out.println("updateTime:" + message.getUpdateTime());
-            System.out.println("uid:" + message.getuId());
             System.out.println("isTag:" + message.getIsTag());
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         }
@@ -59,7 +73,7 @@ public class MessageTest {
     /**
      * 根据传入的map查询相应的消息表
      */
-    @Test
+   
     public void listMessageBy() {
         Map<Object, Object> map = new HashMap<Object, Object>();
         //map.put("id", "aa897350206245f4aeac083865d948c3");
@@ -87,7 +101,6 @@ public class MessageTest {
     /**
      * 根据ID查询消息表
      */
-    @Test
     public void getMessageById() {
         String id = "aa897350206245f4aeac083865d948c3";
         Message message = service.getMessageById(id);
@@ -105,11 +118,12 @@ public class MessageTest {
     /**
      * 根据条件修改消息表,这里是根据id来修改其他项,所以map中必须包含id
      */
-    @Test
+    //@Test
     public void updateMessageById() {
         Map<Object, Object> map = new HashMap<Object, Object>();
-        map.put("id", "aa897350206245f4aeac083865d948c3");
-        map.put("status", 4);
+        map.put("id","a900d71632d74ceab0979464213a005b");
+        map.put("status", 1);
+        map.put("isTag", 1);
         Integer i = service.updateMessageById(map);
         System.out.println(i);
     }
@@ -117,7 +131,6 @@ public class MessageTest {
     /**
      * 删除（根据Id删除消息表）
      */
-    @Test
     public void deleteMessaageById() {
         String id = "ab55154719bc458f9985f84e05034196";
         Integer i = service.deleteMessageById(id);
