6ac49086dcc25e8883b4e85c4cacc1460c977721
ranzhenming <ranzhenming@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-05-09 08:24:46 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@55724 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/quartz/AccountQuartzListener.java
+++ src/main/java/cn/financial/quartz/AccountQuartzListener.java
@@ -32,10 +32,6 @@ public class AccountQuartzListener implements ServletContextListener {
 	
 	private static WebApplicationContext springContext;
 	
-	private OrganizationService organizationService;
-	
-	private UserRoleServiceImpl userRoleServiceImpl;
-	
 	public AccountQuartzListener() {
 		super();
 	}
@@ -45,129 +41,17 @@ public class AccountQuartzListener implements ServletContextListener {
 	}
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		
-		
 		System.out.println("@@@@@@@@@服务开启了");
-		
 		springContext = WebApplicationContextUtils
 				.getWebApplicationContext(sce.getServletContext());
-		
-		organizationService = (OrganizationService) springContext
-				.getBean("OrganizationServiceImpl");
-		
-		userRoleServiceImpl = (UserRoleServiceImpl) springContext
-				.getBean("UserRoleServiceImpl");
-		
 		if ( springContext != null ) {
-			
-			List<Organization> orglist = organizationService.listOrganizationBy(new HashMap<Object,Object>());
-			
-			List<UserRole> rolelist = userRoleServiceImpl.listUserRole(null);
-			
-			Map<Object, Object> map;
-			
-			int year = Calendar.getInstance().get(Calendar.YEAR);
-			int month = Calendar.getInstance().get(Calendar.MONTH);
-			
-			for( int i=0;i<orglist.size();i++ ) {
-				
-				map = new HashMap<Object, Object>();
-				map.put("id", orglist.get(i).getId());
-				
-				if( !organizationService.hasOrganizationSon(map) ) {
-				    Organization rog = organizationService.getCompanyNameBySon(orglist.get(i).getId());
-					if( month+1 == 1 ) {
-						
-						Message message = new Message();
-						message.setId(UuidUtil.getUUID());
-						message.setStatus(0);
-						message.setTheme(1);
-						message.setContent(year+"年1月"+rog.getOrgName()+orglist.get(i).getOrgName()+"预算报表已生成");
-						message.setoId(orglist.get(i).getId());
-						message.setIsTag(0);
-						message.setsName("系统");
-						
-	                    Budget budget = new Budget();
-	                    budget.setId(UuidUtil.getUUID());
-	                    budget.setoId(rog.getId());//分公司id
-	                    budget.setTypeId(orglist.get(i).getId());//部门（根据部门id查分公司id）
-	                    budget.setStatus(2);//提交状态（0 待提交   1已提交  2新增）
-	                    budget.setDelStatus(1);
-	                    budget.setYear(year);
-	                    budget.setMonth(month);
-						
-						String job_budget = "预算表单新增定时器"+i;
-						String job_name = "预算表单消息提示定时器"+i ;
-						
 						try {
-						        
-						        QuartzManager.addJob(QuartzManager.getsched(), job_budget, QuartzBudget.class, "0 0 0 1 1 ? *" , JSONObject
-                                    .fromObject(budget));//新增预算表
-						        
-								QuartzManager.addJob(QuartzManager.getsched(), job_name, QuartzJob.class, "0 0 0 1 1 ? *" , JSONObject
-										.fromObject(message));//消息提醒
-								
+								QuartzManager.addJob(QuartzManager.getsched(), "预算表生成消息提醒任务定时器", QuartzJob.class, "0 31 15 * * ? *",null);//每年一月份一号生成预算表
+								QuartzManager.addJob(QuartzManager.getsched(), "损益表生成消息提醒任务定时器", QuartzBudget.class, "0 31 15 * * ? *",null);//每个月11号生成损益表
+								QuartzManager.addJob(QuartzManager.getsched(), "系统关账消息提醒任务定时器", QuartzStatement.class, "0 31 15 * * ? *",null);//每个月5号生成系统关账提醒
 						} catch (SchedulerException e) {
 								e.printStackTrace();
 						}
-					}
-					
-					Message message = new Message();
-					message.setId(UuidUtil.getUUID());
-					message.setStatus(0);
-					message.setTheme(1);
-					message.setContent(year+"年"+month+"月"+rog.getOrgName()+orglist.get(i).getOrgName()+"损益报表已生成");
-					message.setoId(orglist.get(i).getId());
-					message.setIsTag(0);
-					message.setsName("系统");
-					
-					Statement statement = new Statement();
-                    statement.setId(UuidUtil.getUUID());
-                    statement.setoId(rog.getId());//分公司id
-                    statement.setTypeId(orglist.get(i).getId());//部门（根据部门id查分公司id）
-                    statement.setYear(year);
-                    statement.setMonth(month);
-                    statement.setStatus(2);//提交状态（0 待提交   1已提交  2新增）
-                    statement.setDelStatus(1);
-                    
-					String job_statement = "损益表单新增定时器"+i ;
-					String job_name = "损益表单消息提示定时器"+i ; 
-					
-					try {
-					        QuartzManager.addJob(QuartzManager.getsched(), job_statement, QuartzStatement.class, "0 0 0 11 * ? *" , JSONObject
-                                .fromObject(statement));//新增损益表
-					        
-							QuartzManager.addJob(QuartzManager.getsched(), job_name, QuartzJob.class, "0 0 0 11 * ? *" , JSONObject
-									.fromObject(message));//消息提醒
-							
-					} catch (SchedulerException e) {
-							e.printStackTrace();
-					}
-				 }
-			}
-			for( int i =0;i<rolelist.size();i++ ) {
-				
-				if( rolelist.get(i).getRoleName().equals("制单员") ) {
-					
-					Message message = new Message();
-					message.setId(UuidUtil.getUUID());
-					message.setStatus(0);
-					message.setTheme(1);
-					message.setContent(" 每月10号为系统关账日期，请提前做好相关工作");
-					message.setuId(rolelist.get(i).getuId());
-					message.setIsTag(0);
-					message.setsName("系统");
-					
-					String job_name = "关账消息提示定时器"+i ; 
-					
-					try {
-							QuartzManager.addJob(QuartzManager.getsched(), job_name, QuartzJob.class, "0 0 0 5 * ? *" , JSONObject
-									.fromObject(message));
-					} catch (SchedulerException e) {
-							e.printStackTrace();
-					}
-				}
-			}
 	}else {
 		System.out.println("获取应用程序上下文失败!");
 		return;
