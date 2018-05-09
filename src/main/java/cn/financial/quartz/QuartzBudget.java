a9093d84c1733156d88bacba9ffd73832632541c
ranzhenming <ranzhenming@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-05-09 08:25:43 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@55725 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/quartz/QuartzBudget.java
+++ src/main/java/cn/financial/quartz/QuartzBudget.java
@@ -1,42 +1,79 @@
 package cn.financial.quartz;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.quartz.Job;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import cn.financial.model.Budget;
+import cn.financial.model.Message;
+import cn.financial.model.Organization;
 import cn.financial.service.BudgetService;
-import net.sf.json.JSONObject;
-
+import cn.financial.service.impl.MessageServiceImpl;
+import cn.financial.service.impl.OrganizationServiceImpl;
+import cn.financial.util.UuidUtil;
+/**
+ * 预算表消息生成提醒任务
+ * @author admin
+ *
+ */
 public class QuartzBudget implements Job{
-    
+	private MessageServiceImpl messageService;
+	private OrganizationServiceImpl organizationService;
     private BudgetService budgetService;
+    protected Logger logger = LoggerFactory.getLogger(QuartzBudget.class);
     
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException{
-		
+		messageService = (MessageServiceImpl) AccountQuartzListener.getSpringContext().getBean("MessageServiceImpl");
+		organizationService = (OrganizationServiceImpl) AccountQuartzListener.getSpringContext().getBean("OrganizationServiceImpl");
 	    budgetService = (BudgetService) AccountQuartzListener.getSpringContext().getBean("BudgetServiceImpl");
-	    JobDetail detail = arg0.getJobDetail();
-        JSONObject object = (JSONObject) detail.getJobDataMap().get("budget");
+	    List<Organization> orglist = organizationService.listOrganizationBy(new HashMap<Object,Object>());
+		
+		Map<Object, Object> map;
+		
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		int month = Calendar.getInstance().get(Calendar.MONTH);
 	    try {
-            Budget budget = new Budget();
-            budget.setId(object.getString("id"));
-            budget.setoId(object.getString("oId"));//分公司id
-            budget.setTypeId(object.getString("typeId"));//部门（根据部门id查分公司id）
-            budget.setCreateTime(new Date());
-            budget.setStatus(object.getInt("status"));//提交状态（0 待提交   1已提交  2新增）
-            budget.setDelStatus(object.getInt("delStatus"));
-            budget.setYear(object.getInt("year"));
-            budget.setMonth(object.getInt("month"));
-            Integer flag = budgetService.insertBudget(budget);
-            if (flag == 1) {
-               System.out.println("预算报表数据新增成功");
-            } else {
-                System.out.println("预算报表数据新增失败");
-            }
+	    	for( int i=0;i<orglist.size();i++ ) {
+			map = new HashMap<Object, Object>();
+			map.put("id", orglist.get(i).getId());
+			if( !organizationService.hasOrganizationSon(map) ) {
+				Organization rog = organizationService.getCompanyNameBySon(orglist.get(i).getId());
+		    	Message message = new Message();
+				message.setId(UuidUtil.getUUID());
+				message.setStatus(0);
+				message.setTheme(1);
+				message.setContent(year+"年"+month+"月"+rog.getOrgName()+orglist.get(i).getOrgName()+"预算报表已生成");
+				message.setoId(orglist.get(i).getId());
+				message.setIsTag(0);
+				message.setsName("系统");
+				Integer i1 = messageService.saveMessage(message);
+				if (i1 != 1) {
+					logger.error("发送预算报表消息失败");
+				}
+				Budget budget = new Budget();
+	            budget.setId(UuidUtil.getUUID());
+	            budget.setoId(rog.getId());//分公司id
+	            budget.setTypeId(orglist.get(i).getId());//部门（根据部门id查分公司id）
+	            budget.setStatus(2);//提交状态（0 待提交   1已提交  2新增）
+	            budget.setDelStatus(1);
+	            budget.setYear(year);
+	            budget.setMonth(month);
+	            Integer flag = budgetService.insertBudget(budget);
+	            if (flag != 1) {
+	            	 logger.error("预算报表数据新增失败");
+	            	}
+		    	}
+	    	}
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/cn/financial/quartz/QuartzJob.java
+++ src/main/java/cn/financial/quartz/QuartzJob.java
@@ -1,44 +1,96 @@
 package cn.financial.quartz;
 
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.quartz.Job;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.SchedulerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import cn.financial.controller.OrganizationController;
+import cn.financial.model.Budget;
 import cn.financial.model.Message;
+import cn.financial.model.Organization;
+import cn.financial.model.Statement;
+import cn.financial.model.UserRole;
+import cn.financial.service.BudgetService;
+import cn.financial.service.OrganizationService;
+import cn.financial.service.StatementService;
 import cn.financial.service.impl.MessageServiceImpl;
+import cn.financial.service.impl.OrganizationServiceImpl;
+import cn.financial.service.impl.UserRoleServiceImpl;
+import cn.financial.util.UuidUtil;
 import net.sf.json.JSONObject;
-
+/**
+ * 损益表消息生成提醒任务
+ * @author admin
+ *
+ */
 public class QuartzJob implements Job{
 	
 	private MessageServiceImpl messageService;
-	
+	private OrganizationServiceImpl organizationService;
+	private StatementService statementService;
+    protected Logger logger = LoggerFactory.getLogger(QuartzJob.class);
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException{
-		
 		messageService = (MessageServiceImpl) AccountQuartzListener.getSpringContext().getBean("MessageServiceImpl");
+		organizationService = (OrganizationServiceImpl) AccountQuartzListener.getSpringContext().getBean("OrganizationServiceImpl");
+		statementService = (StatementService) AccountQuartzListener.getSpringContext().getBean("StatementServiceImpl");
+		List<Organization> orglist = organizationService.listOrganizationBy(new HashMap<Object,Object>());
 		
-		JobDetail detail = arg0.getJobDetail();
-		JSONObject object = (JSONObject) detail.getJobDataMap().get("message");
+		Map<Object, Object> map;
 		
-		Message message = new Message();
-		message.setId(object.getString("id"));
-		message.setStatus(object.getInt("status"));
-		message.setContent(object.getString("content"));
-		message.setTheme(object.getInt("theme"));
-		message.setIsTag(object.getInt("isTag"));
-		if(object.getString("oId")!=null) {
-			message.setoId(object.getString("oId"));
-		}
-		if(object.getString("uId")!=null) {
-			message.setuId(object.getString("uId"));
-		}
-		message.setsName(object.getString("sName"));
-		System.out.println(message.getContent()+"content");
-		Integer i = messageService.saveMessage(message);
-		if (i == 1) {
-			System.out.println("发送消息成功！");
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		int month = Calendar.getInstance().get(Calendar.MONTH);
+		
+		try {
+			for( int i=0;i<orglist.size();i++ ) {
+				
+				map = new HashMap<Object, Object>();
+				map.put("id", orglist.get(i).getId());
+				
+				if( !organizationService.hasOrganizationSon(map) ) {
+				    Organization rog = organizationService.getCompanyNameBySon(orglist.get(i).getId());
+					
+					Message message = new Message();
+					message.setId(UuidUtil.getUUID());
+					message.setStatus(0);
+					message.setTheme(1);
+					message.setContent(year+"年"+month+"月"+rog.getOrgName()+orglist.get(i).getOrgName()+"损益报表已生成");
+					message.setoId(orglist.get(i).getId());
+					message.setIsTag(0);
+					message.setsName("系统");
+					Integer i1 = messageService.saveMessage(message);
+					if (i1!= 1) {
+						logger.error("发送损益报表消息失败");
+					}
+					Statement statement = new Statement();
+			        statement.setId(UuidUtil.getUUID());
+			        statement.setoId(rog.getId());//分公司id
+			        statement.setTypeId(orglist.get(i).getId());//部门（根据部门id查分公司id）
+			        statement.setYear(year);
+			        statement.setMonth(month);
+			        statement.setStatus(2);//提交状态（0 待提交   1已提交  2新增）
+			        statement.setDelStatus(1);
+			        Integer flag = statementService.insertStatement(statement);
+			        if(flag!=1) {
+			    	   logger.error("损益报表数据新增失败");
+			       }
+					
+				 }
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("定时任务发生错误");
 		}
 	}
+	
 }
 
