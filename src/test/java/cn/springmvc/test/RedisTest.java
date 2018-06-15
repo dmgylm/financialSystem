e9731a443367e2d7101541af23389bdef6368c07
jinqiquan <jinqiquan@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-06-15 09:57:38 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@58657 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/test/java/cn/springmvc/test/RedisTest.java
+++ src/test/java/cn/springmvc/test/RedisTest.java
@@ -1,21 +1,22 @@
 package cn.springmvc.test;
 
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
 
 import org.bson.Document;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.alibaba.fastjson.JSONArray;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 
+import cn.financial.service.RedisTestService;
 import cn.financial.service.impl.MongoDBServiceImpl;
-import cn.financial.util.RedisUtils;
+//import cn.financial.util.RedisUtils;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:conf/spring.xml", "classpath:conf/spring-mvc.xml",
@@ -24,28 +25,19 @@ import cn.financial.util.RedisUtils;
 
 public class RedisTest {
 	
-	@Autowired
-	public RedisUtils red;
+//	@Autowired
+//	public RedisUtils red;
 	@Autowired
 	public MongoDBServiceImpl mongoDBService;
+	@Autowired
+	public RedisTestService rts;
 	
 	@Test
 	public void test(){
 //		System.out.println(getData());
-		red.insert("test1", getData().toString());
-		System.out.println(red.getData("test1"));
+//		red.insert("test1", getData().toString());
+		JSONArray j = rts.getData();
+		System.out.println(j);
 	}
 	
-	public JSONArray getData(){
-		MongoClient client = new MongoClient("192.168.105.201",27017);
-		MongoCollection<Document> col = client.getDatabase("school").getCollection("personnel");
-		MongoCursor<Document> cur = col.find().iterator();
-		
-		JSONArray json = new JSONArray();
-		while(cur.hasNext()){
-			Document doc = cur.next();
-			json.add(doc);
-		}
-		return json;
-	}
 }
