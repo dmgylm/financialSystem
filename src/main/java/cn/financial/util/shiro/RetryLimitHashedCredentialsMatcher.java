0c25c7e4de9fa8bcaf733b348bd19c9922695999
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-06-21 01:50:03 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@58897 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/util/shiro/RetryLimitHashedCredentialsMatcher.java
+++ src/main/java/cn/financial/util/shiro/RetryLimitHashedCredentialsMatcher.java
@@ -1,7 +1,5 @@
 package cn.financial.util.shiro;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -17,37 +15,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 
-import cn.financial.model.User;
-import cn.financial.util.EhcacheUtil;
 
 public class RetryLimitHashedCredentialsMatcher extends HashedCredentialsMatcher{
     @Autowired
     @Qualifier("apiRedisTemplate")
-    RedisTemplate redis;
+    private RedisTemplate redis;
 
     //private Cache<String, AtomicInteger> passwordRetryCache;
 
     public RetryLimitHashedCredentialsMatcher(CacheManager cacheManager) {
         //passwordRetryCache = cacheManager.getCache("passwordRetryCache");
-    }
-    
-    public void checkLogin(String userName){
-        Object obj=redis.opsForValue().get(userName);//获取错误次数
-        Object locking=redis.opsForValue().get("financialSystem"+"_cache_"+userName+"_status");//获取是否锁定
-        if(locking!=null){
-            //抛出用户锁定异常
-            throw new ExcessiveAttemptsException();
-        }
-        if(obj==null){
-            redis.opsForValue().set(userName, 1, 24, TimeUnit.HOURS);//设置错误次数的缓存key，时间24小时，默认次数第一次
-        }else{
-            int count=(int)obj+1;//错误次数加1
-            if(count>=3){
-                redis.opsForValue().set("financialSystem"+"_cache_"+userName+"_status", "N");//锁定账户
-                throw new ExcessiveAttemptsException();
-            }
-            redis.opsForValue().set(userName, count, 24, TimeUnit.HOURS);//错误次数加1
-        }
     }
     
     @Override
@@ -64,14 +41,30 @@ public class RetryLimitHashedCredentialsMatcher extends HashedCredentialsMatcher
             //if retry count > 3 throw
             throw new ExcessiveAttemptsException();
         }*/
-        
-        checkLogin(username);
+        int count = 0;
+        Object obj=redis.opsForValue().get(username);//获取错误次数
+        Object locking=redis.opsForValue().get("financialSystem"+"_cache_"+username+"_status");//获取是否锁定
+        if(locking!=null){
+            //抛出用户锁定异常
+            throw new ExcessiveAttemptsException();
+        }
+        if(obj==null){
+            redis.opsForValue().set(username, 1, 24, TimeUnit.HOURS);//设置错误次数的缓存key，时间24小时，默认次数第一次
+        }else{
+            count=(int)obj+1;//错误次数加1 
+            redis.opsForValue().set(username, count, 24, TimeUnit.HOURS);//错误次数加1
+        }
         boolean matches = super.doCredentialsMatch(token, info);//密码验证
         System.out.println(matches+"*****************************************");
         if(matches) {
             //clear retry count
             //passwordRetryCache.remove(username);
             redis.delete(username);//登陆成功清除key
+        }else{//判断是否登录成功
+            if(count>=3){//判断登陆失败次数
+                redis.opsForValue().set("financialSystem"+"_cache_"+username+"_status", "N");//锁定账户
+                throw new ExcessiveAttemptsException();
+            }
         }
         return matches;
     }
