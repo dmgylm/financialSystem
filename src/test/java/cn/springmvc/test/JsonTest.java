05961f09de9a2ba51676511a4a169c06e3246523
hushulin <hushulin@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-06-22 05:18:14 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@59083 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/test/java/cn/springmvc/test/JsonTest.java
+++ src/test/java/cn/springmvc/test/JsonTest.java
@@ -22,25 +22,5 @@ public class JsonTest {
 		System.out.println(simplifyJson);
 	}
 
-	@Test
-	public void shortJsonTest() {
-		try {
-			String path = "C:/Users/Admin/Desktop/解析后文件.txt";
-			JSONObject newObj = JsonConvertProcess.simplifyJson(JsonConvertProcess.readFileContent(path));
-			System.out.println(newObj.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	public void mergeJsonTest() {
-		String longTxt = JsonConvertProcess.readFileContent("C:/Users/Admin/Desktop/解析后文件.txt");
-		String shortTxt = JsonConvertProcess.readFileContent("C:/Users/Admin/Desktop/新解析后文件.txt");
-		JSONObject longJson = JSONObject.fromObject(longTxt);
-		JSONObject shortJson = JSONObject.fromObject(shortTxt);
-		JSONObject mergeJson = JsonConvertProcess.mergeJson(longJson,shortJson);
-		System.out.println(mergeJson);
-	}
 	
 }
