6c5c5ffb9e4826c87c49b2deec605e90881f93f1
zhouchunping <zhouchunping@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-06-25 09:37:30 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@59280 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/service/RedisCacheService.java
+++ src/main/java/cn/financial/service/RedisCacheService.java
@@ -4,13 +4,39 @@ import org.springframework.cache.Cache;
 
 public interface RedisCacheService {
 	
+	/**
+	 * 删除Cache
+	 * @param cacheName
+	 */
 	public void removeAll(String cacheName);
 	
+	/**
+	 * 根据Key删除单个缓存
+	 * @param cacheName 缓存名称
+	 * @param key 缓存Key
+	 */
 	public void remove(String cacheName,String key);
 	
+	/**
+	 * 添加缓存
+	 * @param cacheName 缓存名称
+	 * @param key 缓存Key
+	 * @param value
+	 */
 	public void put(String cacheName,String key,Object value);
 	
+	/**
+	 * 根据Key获取缓存内容
+	 * @param cacheName 缓存名称
+	 * @param key 缓存Key
+	 * @return
+	 */
 	public Object get(String cacheName, String key);
 
+	/**
+	 * 获取缓存对象
+	 * @param cacheName
+	 * @return
+	 */
 	public Cache get(String cacheName);
 }
