aa58925d1c4466e5275c6a7b6dfcf229b0c954b8
zhouchunping <zhouchunping@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-06-28 01:39:25 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@59548 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/util/ElementConfig.java
+++ src/main/java/cn/financial/util/ElementConfig.java
@@ -2,51 +2,83 @@ package cn.financial.util;
 
 public class ElementConfig {
 
-    // 成功
+    /**
+     * 成功
+     */
     public static final String RUN_SUCCESSFULLY = "RUN_SUCCESSFULLY";
 
-    // 失败
+    /**
+     * 失败
+     */
     public static final String RUN_ERROR = "RUN_ERROR";
 
-    // 系统错误
+    /**
+     * 系统错误
+     */
     public static final String RUN_FAILURE = "RUN_FAILURE";
 
-    // 该用户不存在
+    /**
+     * 该用户不存在
+     */
     public static final String LOGIN_NO_USER = "LOGIN_NO_USER";
 
-    // 密码或账户错误
+    /**
+     * 密码或账户错误
+     */
     public static final String LOGIN_USERNAME_ERROR = "LOGIN_USERNAME_ERROR";
 
-    // 账户已锁
+    /**
+     * 账户已锁
+     */
     public static final String LOGIN_USER_LOCKOUT = "LOGIN_USER_LOCKOUT";
 
-    // 其他错误
+    /**
+     * 其他错误
+     */
     public static final String LOGIN_FAILURE = "LOGIN_FAILURE";
 
-    // 文件大于5M
+    /**
+     * 文件大于5M
+     */
     public static final String CAPITAL_FILE_EXCEED_5M = "CAPITAL_FILE_EXCEED_5M";
 
-    // 新旧密码一致
+    /**
+     * 新旧密码一致
+     */
     public static final String USER_OLDPWD = "USER_OLDPWD";
 
-    // 原密码输入错误
+    /**
+     * 原密码输入错误
+     */
     public static final String USER_OLDPWD_ERROR = "USER_OLDPWD_ERROR";
 
-    // 密码输入格式错误
+    /**
+     * 密码输入格式错误
+     */
     public static final String USER_PWDFORMAT_ERROR = "USER_PWDFORMAT_ERROR";
 
-    // 用户名已存在
+    /**
+     * 用户名已存在
+     */
     public static final String USERNAME_EXISTENCE = "USERNAME_EXISTENCE";
     
-    // 色名称不能重复
+    /**
+     * 色名称不能重复
+     */
     public static final String ROLENAME_EXISTENCE = "ROLENAME_EXISTENCE";
     
-    //重置密码
+    /**
+     * 重置密码
+     */
     public static final String RESET_PWD = "RESET_PWD";
     
-    //工号已存在
+    /**
+     * 工号已存在
+     */
     public static final String JOBNUMBER_EXISTENCE = "JOBNUMBER_EXISTENCE";
     
-    // 此节点下存在未停用的节点，请先停用子节点
+    /**
+     * 此节点下存在未停用的节点，请先停用子节点
+     */
     public static final String ORGANIZA_DELEFALSE = "ORGANIZA_DELEFALSE";
 }
