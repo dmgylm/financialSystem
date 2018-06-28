5c103decb015cc3aa5b958c0247401dda5d909ee
hushulin <hushulin@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-06-28 08:58:38 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@59654 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/service/impl/OrganizationServiceImpl.java
+++ src/main/java/cn/financial/service/impl/OrganizationServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.financial.service.impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -532,12 +533,38 @@ public class OrganizationServiceImpl implements OrganizationService {
         return organizationDAO.getCompany();
     }
 
-    public List<Organization> listOrganizationcode(List<String> listmap) {
-        return organizationDAO.listOrganizationcode(listmap);
-    }
 
-    public List<Organization> listOrganization(List<String> list) {
+
+	@Override
+	public List<Organization> listOrganization(List<String> ids) {
+		  List<Organization> list = organizationDAO.listOrganization(ids);
+		  List<Organization> codeSonList = new ArrayList<Organization>();
+	      List<String> listmap = new ArrayList<String>();
+	      for (Organization organization : list) {
+	          String his_permission = organization.getHis_permission();
+	          String[] hps = his_permission.split(",");// 分割逗号
+	          listmap.addAll(Arrays.asList(hps));// 所有的his_permission存到listmap当中
+	      }
+	      // 查询对应的节点的数据
+	     List<Organization> listshow = organizationDAO.listOrganizationcode(listmap);
+	     //根据每个节点id查询对应的子节点数据
+	     for (int i = 0; i < listshow.size(); i++) {
+	   	 List<Organization> codeSon =listTreeByIdForSon(listshow.get(i).getId());
+			for (int j = 0; j < codeSon.size(); j++) {
+				codeSonList.add(codeSon.get(j));
+			}
+		}
+
+	     
+	      return codeSonList;
+	
+	}
+	 /*   public List<Organization> listOrganizationcode(List<String> listmap) {
+    return organizationDAO.listOrganizationcode(listmap);
+     }
+     public List<Organization> listOrganization(List<String> list) {
         return organizationDAO.listOrganization(list);
-    }
+    }*/
 
+	
 }
