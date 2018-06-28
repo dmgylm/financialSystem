55881fa99b88cf0d76f3d84fea75631fd18607ba
chengsen <chengsen@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-06-28 01:38:11 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@59545 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/controller/OrganizationMoveController.java
+++ src/main/java/cn/financial/controller/OrganizationMoveController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.financial.model.OrganizationMove;
 import cn.financial.service.OrganizationMoveService;
@@ -34,6 +35,7 @@ public class OrganizationMoveController {
 	 * @param response
 	 * @return
 	 */
+	@ResponseBody
 	@RequestMapping(value = "/listBy", method = RequestMethod.POST)
 	public Map<String, Object> listOrganizationMoveBy(HttpServletRequest request,HttpServletResponse response){
 		
