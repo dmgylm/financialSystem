52ac6b290217357fbe54991a5d85d6ad2b53c2e6
chengsen <chengsen@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-06-29 08:18:53 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@59779 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

---
+++ src/main/java/cn/financial/webSocket/FinancialSocketHandler.java
@@ -0,0 +1,98 @@
+package cn.financial.webSocket;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+
+public class FinancialSocketHandler implements WebSocketHandler {
+	
+	private static final Logger logger;
+	
+	private static final ArrayList<WebSocketSession> users;
+	
+	static {
+        users = new ArrayList<WebSocketSession>();
+        logger = Logger.getLogger(FinancialSocketHandler.class);
+    }
+	
+	/**
+	 * 链接成功时触发
+	 */
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		logger.info("connect to the websocket success......");
+		users.add(session);
+	}
+	
+	/**
+	 * 关闭链接时触发
+	 */
+	public void afterConnectionClosed(WebSocketSession session,CloseStatus closeStatus) throws Exception {
+		logger.info("websocket connection closed......");
+		users.remove(session);
+	}
+	
+	/**
+	 * js调用websocket.send时候，会调用该方法
+	 */
+	public void handleMessage(WebSocketSession session,WebSocketMessage<?> socketMessage) throws Exception {
+		logger.info("进入send实现类中，传回数据");
+	}
+	
+	public void handleTransportError(WebSocketSession session,Throwable throwable) throws Exception {
+		logger.info("websocket connection closed......");
+		users.remove(session);
+	}
+	
+	public boolean supportsPartialMessages() {
+		return false;
+	}
+	
+	/**
+	 * 给某个用户发送消息
+	 * @param userName
+	 * @param message
+	 */
+	public void sendMessageToUser(String userName,TextMessage message) {
+		
+		for(WebSocketSession user : users) {
+			if(user.getUri().toString().substring(user.getUri().toString().lastIndexOf("/")+1).equals(userName)) {
+				try {
+					if(user.isOpen()) {
+						synchronized (user) {
+							user.sendMessage(message);
+						}
+					}
+				}catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	/**
+	 * 给所有在线用户发送消息
+	 * @param message
+	 */
+	public void sendMessageToUsers(TextMessage message) {
+		
+		for(WebSocketSession user : users) {
+			try {
+				if(user.isOpen()) {
+					user.sendMessage(message);
+				}
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
---
+++ src/main/java/cn/financial/webSocket/FinancialSocketHandshakeInterceptor.java
@@ -0,0 +1,36 @@
+package cn.financial.webSocket;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+public class FinancialSocketHandshakeInterceptor implements HandshakeInterceptor {
+
+	@Override
+	public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler webSocketHandler, Exception ex) {
+		
+	}
+
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler webSocketHandler,
+			Map<String, Object> attributes) throws Exception {
+		
+		if(request instanceof ServletServerHttpRequest) {
+			ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+			HttpSession session = servletRequest.getServletRequest().getSession(false);
+			if(session != null) {
+				//使用userName区分WebSocketHandler，以便定向发送消息
+                //String userName = (String)session.getAttribute("SESSION_USERNAME");
+                //attributes.put("WEBSOCKET_USERNAME",userName);
+			}
+		}
+		return true;
+	}
+
+}
