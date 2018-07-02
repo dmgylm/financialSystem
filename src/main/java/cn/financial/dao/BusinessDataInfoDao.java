21ee0fe192b2388e4386907e3a3e652a670c89d6
ranzhenming <ranzhenming@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-07-02 05:09:26 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@59857 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

---
+++ src/main/java/cn/financial/dao/BusinessDataInfoDao.java
@@ -0,0 +1,49 @@
+package cn.financial.dao;
+
+import java.util.List;
+import java.util.Map;
+import cn.financial.model.BusinessData;
+import cn.financial.model.BusinessDataInfo;
+
+/**
+ * 损益表 Dao
+ * @author Lmn
+ *
+ */
+public interface BusinessDataInfoDao {
+    
+    /**
+     * 新增损益从表数据
+     * @param businessData
+     * @return 
+     */
+    Integer insertBusinessDataInfo(BusinessDataInfo businessData);
+    
+    /**
+     * 删除损益从表数据 （status=0）
+     * @param businessData
+     * @return
+     */
+    Integer deleteBusinessDataInfo(String id);
+    
+    /**
+     * 修改损益从表数据
+     * @param businessData
+     * @return
+     */
+    Integer updateBusinessDataInfo(BusinessDataInfo businessData);  
+   
+    /**
+     * 查询损益从表所有的数据
+     * @return
+     */
+    List<BusinessData> getAll(Map<Object, Object> map);
+     
+    /**
+     * 根据id查询损益表数据
+     * @param id
+     * @return
+     */
+    BusinessDataInfo selectBusinessDataById(String id);
+     
+}
