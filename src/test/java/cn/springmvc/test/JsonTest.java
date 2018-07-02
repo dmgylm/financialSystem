12557adeb9001dcf9bac115e25c765c39f10377c
hushulin <hushulin@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-07-02 07:11:08 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@59883 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/test/java/cn/springmvc/test/JsonTest.java
+++ src/test/java/cn/springmvc/test/JsonTest.java
@@ -1,12 +1,15 @@
 package cn.springmvc.test;
 
 import net.sf.json.JSONObject;
+
 import org.junit.Test;
+
 import cn.financial.util.JsonConvertProcess;
+import cn.financial.util.JsonDataProcess;
 
 public class JsonTest {
 	
-/*    @Test
+    @Test
 	 public  void  shortJson(){
 		String path = "C:/Users/whg/Desktop/解析后文件.txt";
 		JSONObject newObj = JSONObject.fromObject(JsonConvertProcess.readFileContent(path));
@@ -14,9 +17,9 @@ public class JsonTest {
 		JSONObject json=JSONObject.fromObject(JsonConvertProcess.readFileContent(paths));
 		String jsons=JsonConvertProcess.readFileContent(paths).toString();//传过来 的数据先用文件代替
 		JSONObject mergin=JsonConvertProcess.mergeJson(newObj,json);
-		JSONObject simplifyJson=JsonConvertProcess.simplifyJson(jsons,mergin.toString());
+		JSONObject simplifyJson=JsonDataProcess.simplifyJson(mergin.toString(),jsons);
 		System.out.println(simplifyJson);
-	}*/
+	}
 
 	
 }
