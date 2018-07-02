91691b2e188e1544665832793fcb85f0d9df24e3
ranzhenming <ranzhenming@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-07-02 05:10:29 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@59860 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/quartz/QuartzBudget.java
+++ src/main/java/cn/financial/quartz/QuartzBudget.java
@@ -10,91 +10,127 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import cn.financial.model.Message;
 import cn.financial.model.Organization;
 import cn.financial.model.BusinessData;
-import cn.financial.service.BusinessDataService;
-import cn.financial.service.MessageService;
-import cn.financial.service.OrganizationService;
+import cn.financial.model.BusinessDataInfo;
+import cn.financial.model.DataModule;
+import cn.financial.service.impl.BusinessDataInfoServiceImpl;
 import cn.financial.service.impl.BusinessDataServiceImpl;
+import cn.financial.service.impl.DataModuleServiceImpl;
 import cn.financial.service.impl.MessageServiceImpl;
 import cn.financial.service.impl.OrganizationServiceImpl;
+import cn.financial.util.JsonConvertProcess;
 import cn.financial.util.UuidUtil;
+
 /**
  * 预算表消息生成提醒任务
+ * 
  * @author admin
  *
  */
-public class QuartzBudget implements Job{
-    
-    private MessageServiceImpl messageService;
+@Component
+public class QuartzBudget implements Job {
+	private MessageServiceImpl messageService;
+	private OrganizationServiceImpl organizationService;
+	private BusinessDataServiceImpl businessDataService;
+	protected Logger logger = LoggerFactory.getLogger(QuartzBudget.class);
+	private DataModuleServiceImpl dataModuleServiceImpl;
+	private BusinessDataInfoServiceImpl businessDataInfoService;
 
-    private OrganizationServiceImpl organizationService;
-
-    private BusinessDataServiceImpl businessDateService;
-    
-    protected Logger logger = LoggerFactory.getLogger(QuartzBudget.class);
-    
 	@Override
-	public void execute(JobExecutionContext arg0) throws JobExecutionException{
-        messageService = (MessageServiceImpl) AccountQuartzListener.getSpringContext().getBean("MessageServiceImpl");
-        organizationService = (OrganizationServiceImpl) AccountQuartzListener.getSpringContext().getBean("OrganizationServiceImpl");
-        businessDateService = (BusinessDataServiceImpl) AccountQuartzListener.getSpringContext().getBean("BusinessDataServiceImpl");
-		List<Organization> orglist = organizationService.listOrganizationBy(new HashMap<Object,Object>());
-		List<Organization> orgCompany=organizationService.getCompany();
-		Map<Object, Object> map;
-		
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		long start = System.currentTimeMillis();
+		messageService = (MessageServiceImpl) AccountQuartzListener.getSpringContext().getBean("MessageServiceImpl");
+		organizationService = (OrganizationServiceImpl) AccountQuartzListener.getSpringContext()
+				.getBean("OrganizationServiceImpl");
+		businessDataService = (BusinessDataServiceImpl) AccountQuartzListener.getSpringContext()
+				.getBean("BusinessDataServiceImpl");
+		dataModuleServiceImpl = (DataModuleServiceImpl) AccountQuartzListener.getSpringContext()
+				.getBean("DataModuleServiceImpl");
+		businessDataInfoService = (BusinessDataInfoServiceImpl) AccountQuartzListener.getSpringContext()
+				.getBean("BusinessDataInfoServiceImpl");
+		System.out.println("启动业务表定时添加任务");
+
+		String reportType = ""; // reportType，数据模板报表类型。DataModule.REPORT_TYPE_BUDG预算，DataModule.REPORT_TYPE_PROFIT_LOSS损益
+		String msgType = ""; // 消息报表类型损益还是预算
+		int bunType = 0; // 业务表类型。 1损益2预算
+		int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);// 获取当前系统时间几号
+		if (day == 1) {// 预算
+			reportType = DataModule.REPORT_TYPE_BUDGET;
+			msgType = "预算";
+			bunType = 1;
+		} else if (day == 11) {
+			reportType = DataModule.REPORT_TYPE_PROFIT_LOSS;
+			msgType = "损益";
+			bunType = 2;
+		}
+		List<Organization> orgDep = organizationService.getDep();// 获取所有部门
+		List<Organization> orgCompany = organizationService.getCompany();// 获取所有公司
+		Map<Object, Object> mo = new HashMap<>();
+		mo.put("status", 1);
+		mo.put("reportType", reportType);
+		List<DataModule> ldm = dataModuleServiceImpl.listDataModule(mo);
 		int year = Calendar.getInstance().get(Calendar.YEAR);
 		int month = Calendar.getInstance().get(Calendar.MONTH);
-	    try {for (int i = 0; i < orgCompany.size(); i++) {
-			Message message = new Message();
-			message.setId(UuidUtil.getUUID());
-			message.setStatus(0);
-			message.setTheme(1);
-			message.setContent(year+"年"+month+"月"+orgCompany.get(i).getOrgName()+"预算表已生成");
-			message.setoId(orgCompany.get(i).getId());
-			message.setIsTag(0);
-			message.setsName("系统");
-			Integer i1 = messageService.saveMessage(message);
-			if (i1!= 1) {
-				logger.error("发送损益报表消息失败");
+		try {
+			for (int i = 0; i < orgCompany.size(); i++) {
+				Message message = new Message();
+				message.setId(UuidUtil.getUUID());
+				message.setStatus(0);
+				message.setTheme(1);
+				message.setContent(year + "年" + month + "月" + orgCompany.get(i).getOrgName() + msgType + "表已生成");
+				message.setoId(orgCompany.get(i).getId());
+				message.setIsTag(0);
+				message.setsName("系统默认");
+				Integer i1 = messageService.saveMessage(message);
+				if (i1 != 1) {
+					logger.error(msgType + "报表发送消息失败");
+				}
 			}
+			for (int k = 0; k < ldm.size(); k++) {// 获取所有预算表数据模板
+				for (int i = 0; i < orgDep.size(); i++) { // 获取所有部门
+					Organization orgP = organizationService.getOrgaUpFromOne(orgDep.get(i).getId(),
+							ldm.get(k).getBusinessType());// 获取对应业务板块层数据
+					if (orgP != null) {
+						Organization org = organizationService.getCompanyNameBySon(orgDep.get(i).getId());// 获取对应部门的公司
+						if (org != null) {
+							BusinessData statement = new BusinessData();
+							String sid = UuidUtil.getUUID();
+							statement.setId(sid);
+							statement.setoId(org.getId());// 分公司id
+							statement.setTypeId(orgDep.get(i).getId());// 部门（根据部门id查分公司id）
+							statement.setYear(year);
+							statement.setMonth(month);
+							statement.setStatus(2);// 提交状态（0 待提交 1已提交 2新增）
+							statement.setDelStatus(1);
+							statement.setsId(bunType);// 1表示损益表 2表示预算表
+							statement.setDataModuleId(ldm.get(k).getId());// 数据模板id
+
+							Integer flag = businessDataService.insertBusinessData(statement);
+							if (flag != 1) {
+								logger.error(msgType + "主表数据新增失败");
+							} else {
+								BusinessDataInfo sdi = new BusinessDataInfo();
+								sdi.setId(UuidUtil.getUUID());
+								sdi.setBusinessDataId(sid);
+								sdi.setInfo(JsonConvertProcess.simplifyJson(ldm.get(k).getModuleData()).toString());
+								Integer flagt = businessDataInfoService.insertBusinessDataInfo(sdi);
+								if (flagt != 1) {
+									logger.error(msgType + "从表数据新增失败");
+								}
+							}
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-	    	for( int i=0;i<orglist.size();i++ ) {
-			map = new HashMap<Object, Object>();
-			map.put("id", orglist.get(i).getId());
-			if( !organizationService.hasOrganizationSon(map) ) {
-				Organization rog = organizationService.getCompanyNameBySon(orglist.get(i).getId());
-//				Budget budget = new Budget();
-//	            budget.setId(UuidUtil.getUUID());
-//	            budget.setoId(rog.getId());//分公司id
-//	            budget.setTypeId(orglist.get(i).getId());//部门（根据部门id查分公司id）
-//	            budget.setStatus(2);//提交状态（0 待提交   1已提交  2新增）
-//	            budget.setDelStatus(1);
-//	            budget.setYear(year);
-//	            budget.setMonth(month);
-//	            Integer flag = budgetService.insertBudget(budget);
-				BusinessData statement = new BusinessData();
-                statement.setId(UuidUtil.getUUID());
-                statement.setoId(rog.getId());//分公司id
-                statement.setTypeId(orglist.get(i).getId());//部门（根据部门id查分公司id）
-                statement.setYear(year);
-                statement.setMonth(month);
-                statement.setStatus(2);//提交状态（0 待提交   1已提交  2新增）
-                statement.setDelStatus(1);
-                statement.setsId(2);//1表示损益表   2表示预算表
-                Integer flag = businessDateService.insertBusinessData(statement);
-	            if (flag != 1) {
-	            	 logger.error("预算报表数据新增失败");
-	            	}
-		    	}
-	    	}
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		long end = System.currentTimeMillis();
+		System.out.println("时间花费~~~~");
+		System.out.println(end - start);
 	}
 }
-
