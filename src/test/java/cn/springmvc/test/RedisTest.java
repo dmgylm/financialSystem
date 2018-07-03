815215aabb039b7e6bd5ee90e0f727093ce60cd0
jinqiquan <jinqiquan@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-07-03 08:09:14 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@59981 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/test/java/cn/springmvc/test/RedisTest.java
+++ src/test/java/cn/springmvc/test/RedisTest.java
@@ -1,31 +1,30 @@
 package cn.springmvc.test;
 
 
+import java.util.List;
+import java.util.Map;
+
 import org.bson.Document;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.Cache;
-import org.springframework.cache.Cache.ValueWrapper;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.redis.cache.RedisCacheElement;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import redis.clients.jedis.Jedis;
 
 import com.alibaba.fastjson.JSONArray;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 
-import cn.financial.service.RedisCacheService;
+import cn.financial.model.InCopy;
 import cn.financial.service.RedisTestService;
+import cn.financial.service.impl.InCopyServiceImpl;
 import cn.financial.service.impl.MongoDBServiceImpl;
 //import cn.financial.util.RedisUtils;
-import cn.financial.util.EhcacheUtil;
+import cn.financial.util.RedisUtils;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring/spring.xml", "classpath:spring/spring-mvc.xml",
@@ -40,25 +39,46 @@ public class RedisTest {
 	public MongoDBServiceImpl mongoDBService;
 	@Autowired
 	public RedisTestService rts;
-	
 	@Autowired
-	private RedisCacheService cache;
+	public InCopyServiceImpl ii;
+	@Autowired
+	public RedisUtils redisUtils;
 	
 	
-	@Test
-	public void testRedisCache(){
-		cache.put("testCache", "test1", 233333);//添加缓存
-		cache.put("testCache", "test2", 233333);//添加缓存
-		cache.remove("testCache", "test1");//删除单个缓存KEY
-		cache.removeAll("testCache");//删除整个Cache
-	}
-
 	@Test
 	public void test(){
 //		System.out.println(getData());
 //		red.insert("test1", getData().toString());
-		JSONArray j = rts.getData();
-		System.out.println(j);
+		long a = System.currentTimeMillis();
+		rts.delete();
+//		rts.get1();
+		long b = System.currentTimeMillis();
+		System.out.println("----------------------------------------------------------------------");
+		System.out.println(b-a);
+//		System.out.println(t);
+	}
+	
+	@Test
+	public void test1(){
+//		List<String> listId = ii.getCopyIdList();
+		long a = System.currentTimeMillis();
+		List<InCopy> list = ii.getCopyId();
+		for(InCopy in : list){
+			rts.get(in);
+		}
+		long b = System.currentTimeMillis();
+		System.out.println(b-a);
+	}
+	
+	@Test
+	public void test2(){
+		List<String> a = rts.getListValue("ceshi123");
+		System.out.println(a);
+	}
+	
+	@Test
+	public void testt(){
+		System.gc();
 	}
 	
 }
