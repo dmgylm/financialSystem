786aff480be0f51953d5b3beae578e9d5eb60496
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-07-05 09:52:02 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@60296 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/service/impl/RoleResourceServiceImpl.java
+++ src/main/java/cn/financial/service/impl/RoleResourceServiceImpl.java
@@ -1,17 +1,23 @@
 package cn.financial.service.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import cn.financial.dao.RoleResourceDAO;
 import cn.financial.model.RoleResource;
+import cn.financial.model.UserRole;
 import cn.financial.service.RoleResourceService;
+import cn.financial.service.UserRoleService;
 import cn.financial.util.TreeNode;
 
 /**
@@ -23,6 +29,8 @@ import cn.financial.util.TreeNode;
 public class RoleResourceServiceImpl implements RoleResourceService{
     @Autowired
     private RoleResourceDAO roleResourceDao;
+    @Autowired
+    private UserRoleService userRoleService;
     /**
      * 查询所有/根据角色id查对应的功能权限
      * @return
@@ -36,6 +44,7 @@ public class RoleResourceServiceImpl implements RoleResourceService{
      * @return
      */
     @Override
+    @CacheEvict(value = "find_Roles_Permissions", allEntries = true)
     public Integer insertRoleResource(RoleResource roleResource) {
         return roleResourceDao.insertRoleResource(roleResource);
     }
@@ -43,6 +52,7 @@ public class RoleResourceServiceImpl implements RoleResourceService{
      * 删除
      */
     @Override
+    @CacheEvict(value = "find_Roles_Permissions", allEntries = true)
     public Integer deleteRoleResource(String rId) {
         return roleResourceDao.deleteRoleResource(rId);
     }
@@ -50,33 +60,46 @@ public class RoleResourceServiceImpl implements RoleResourceService{
      * 修改
      */
     @Override
+    @CacheEvict(value = "find_Roles_Permissions", allEntries = true)
     public Integer updateRoleResource(RoleResource roleResource) {
         return roleResourceDao.updateRoleResource(roleResource);
     } 
     
     /**
-     * 根据角色id查询对应功能权限关联信息（必须勾选父节点，父节点相当于查看权限）
+     * 根据用户名查询对应角色功能权限信息
      * @param roleId
      * @return
      */
-    public JSONObject roleResourceList(String roleId){
-        List<RoleResource> roleResource = roleResourceDao.listRoleResource(roleId);
-        List<TreeNode<RoleResource>> nodes = new ArrayList<>();
-        JSONObject jsonObject = null;
-        if(!CollectionUtils.isEmpty(roleResource)){
-            for (RoleResource rss : roleResource) {
-                TreeNode<RoleResource> node = new TreeNode<>();
-                node.setId(rss.getCode().toString());//当前code
-                String b=rss.getParentId().substring(rss.getParentId().lastIndexOf("/")+1);
-                node.setParentId(b);//父id
-                node.setName(rss.getName());//功能权限名称
-                node.setPid(rss.getsId());//当前权限id
-                // node.setNodeData(rss);
-                nodes.add(node);
+    public List<JSONObject> roleResourceList(String userName){
+        List<JSONObject> jsonObject = new ArrayList<>();
+        Map<String, TreeNode<RoleResource>>  map=new HashMap<>();
+        List<UserRole> userRole = userRoleService.listUserRole(userName);//根据用户名查询对应角色信息
+        if(userRole.size()>0){//用户可能拥有多个角色，需要去重权限信息
+            for(UserRole list:userRole){
+              //根据角色id查询对应功能权限关联信息（必须勾选父节点，父节点相当于查看权限）
+                List<RoleResource> roleResource = roleResourceDao.listRoleResource(list.getrId());
+                List<TreeNode<RoleResource>> nodes = new ArrayList<>();
+                if(!CollectionUtils.isEmpty(roleResource)){
+                    for (RoleResource rss : roleResource) {
+                        TreeNode<RoleResource> node = new TreeNode<>();
+                        node.setId(rss.getCode().toString());//当前code
+                        String b=rss.getParentId().substring(rss.getParentId().lastIndexOf("/")+1);
+                        node.setParentId(b);//父id
+                        node.setName(rss.getName());//功能权限名称
+                        node.setPid(rss.getsId());//当前权限id
+                        // node.setNodeData(rss);
+                        nodes.add(node);
+                        map.put(node.getPid(), node);
+                    }
+                }
             }
-            jsonObject = (JSONObject) JSONObject.toJSON(TreeNode.buildTree(nodes));
+         }
+        List<TreeNode<RoleResource>> roleList=new ArrayList<>();
+        for (TreeNode<RoleResource> item : map.values()) {
+            roleList.add(item);
         }
+        jsonObject.add((JSONObject) JSONObject.toJSON(TreeNode.buildTree(roleList)));
         return jsonObject;
-    }
+     }
 }
  
