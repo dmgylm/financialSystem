064897575fb287132c309bc6929ddfe9ca7e77dd
chengsen <chengsen@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-07-05 07:27:33 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@60261 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/quartz/AccountQuartzListener.java
+++ src/main/java/cn/financial/quartz/AccountQuartzListener.java
@@ -1,25 +1,11 @@
 package cn.financial.quartz;
 
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import org.quartz.SchedulerException;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
-
-import cn.financial.model.Message;
-import cn.financial.model.Organization;
-import cn.financial.model.BusinessData;
-import cn.financial.model.UserRole;
-import cn.financial.service.OrganizationService;
-import cn.financial.service.impl.UserRoleServiceImpl;
-import cn.financial.util.UuidUtil;
-import net.sf.json.JSONObject;
 
 
 public class AccountQuartzListener implements ServletContextListener {
@@ -49,9 +35,10 @@ public class AccountQuartzListener implements ServletContextListener {
 							
 								//QuartzManager.addJob(QuartzManager.getsched(), "预算表生成消息提醒任务定时器", QuartzBudget.class, "0 0 0 1 1 ?",null);//每年一月份一号生成预算表
 								QuartzManager.addJob(QuartzManager.getsched(), "损益表生成消息提醒任务定时器", QuartzBudget.class, "0 0 0 11 * ?",null);//每个月11号生成损益表
-								QuartzManager.addJob(QuartzManager.getsched(), "系统停止录入数据截止日期提醒任务定时器", QuartzStatement.class, " 0 0 0 5 * ?",null);//每个月5号生成系统关账提醒
+								QuartzManager.addJob(QuartzManager.getsched(), "系统停止录入数据截止日期提醒任务定时器", QuartzStatement.class, "0 0 0 5 * ?",null);//每个月5号生成系统关账提醒
+						        QuartzManager.addJob(QuartzManager.getsched(), "密码有效时间到期提醒任务定时器", QuartzPwdTime.class, "0 0 9 * * ?" , null );
 						} catch (SchedulerException e) {
-								e.printStackTrace();
+							e.printStackTrace();
 						}
 	}else {
 		System.out.println("获取应用程序上下文失败!");
