496509a56e56300b38ebc07d8512d8f948dfdc77
hushulin <hushulin@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-07-05 03:13:49 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@60232 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/test/java/cn/springmvc/test/JsonTest.java
+++ src/test/java/cn/springmvc/test/JsonTest.java
@@ -1,8 +1,10 @@
 package cn.springmvc.test;
 
-import net.sf.json.JSONObject;
+
 
 import org.junit.Test;
+
+import com.alibaba.fastjson.JSONObject;
 
 import cn.financial.util.JsonConvertProcess;
 import cn.financial.util.JsonDataProcess;
@@ -12,9 +14,9 @@ public class JsonTest {
     @Test
 	 public  void  shortJson(){
 		String path = "C:/Users/whg/Desktop/解析后文件.txt";
-		JSONObject newObj = JSONObject.fromObject(JsonConvertProcess.readFileContent(path));
+		JSONObject newObj = JSONObject.parseObject(JsonConvertProcess.readFileContent(path));
 		String paths="C:/Users/whg/Desktop/解析后文件测试.txt";
-		JSONObject json=JSONObject.fromObject(JsonConvertProcess.readFileContent(paths));
+		JSONObject json=JSONObject.parseObject(JsonConvertProcess.readFileContent(paths));
 		String jsons=JsonConvertProcess.readFileContent(paths).toString();//传过来 的数据先用文件代替
 		JSONObject mergin=JsonConvertProcess.mergeJson(newObj,json);
 		JSONObject simplifyJson=JsonDataProcess.simplifyJson(mergin.toString(),jsons);
