306c235dd8a84873f96d86874a27fc12b17c76a4
zhouchunping <zhouchunping@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-07-10 01:52:33 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@60497 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

---
+++ src/test/java/cn/springmvc/test/BudgetHtmlTest.java
@@ -0,0 +1,70 @@
+package cn.springmvc.test;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import cn.financial.exception.FormulaAnalysisException;
+import cn.financial.util.HtmlAnalysis;
+import cn.financial.util.HtmlGenerate;
+import cn.financial.util.JsonConvertProcess;
+
+@RunWith(JUnit4.class)
+public class BudgetHtmlTest {
+	
+	@Test
+	public void genBudgetTempJson(){
+		String path = "D:/Project/fmss/Excel报表/2018年管理报表模板（20180319）-最新/2018年管理报表模板（20180319）-最新/车管家业务预算.xls";
+		AnalysisExcelFormula aef = new AnalysisExcelFormula(path );
+		String json = aef.analysisExcel();
+//		System.out.println(json);
+		
+		HtmlGenerate hg = new HtmlGenerate();
+		String html = hg.generateHtml(json, HtmlGenerate.HTML_TYPE_TEMPLATE);
+		System.out.println(html);
+		
+		HtmlAnalysis ha = new HtmlAnalysis(html, 1, 2, 2, 3);
+		try {
+			String htmlJson = ha.analysis();
+//			System.out.println(htmlJson);
+//			String generateHtml = hg.generateHtml(htmlJson, HtmlGenerate.HTML_TYPE_INPUT);
+//			System.out.println(generateHtml);
+		} catch (FormulaAnalysisException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void analysisHtml(){
+		HtmlAnalysis ha = new HtmlAnalysis(new File("C:/Users/Admin/Desktop/Html budget.html"), null, 2, 1, 2);
+		try {
+			String json = ha.analysis();
+			
+			JSONObject jsonObj = JSONObject.parseObject(json);
+			
+			JsonConvertProcess jcp = new JsonConvertProcess();
+			JSONObject budgetJson = jcp.generateMonthlyBudgetJson(jsonObj);//生成预算模板数据
+			
+			JSONObject simplifyJson = JsonConvertProcess.simplifyJson(budgetJson);
+			System.out.println(simplifyJson);
+//			System.out.println(budgetJson);
+//			System.out.println(json);
+//			HtmlGenerate hg = new HtmlGenerate();
+//			String html = hg.generateHtml(budgetJson.toJSONString(), HtmlGenerate.HTML_TYPE_TEMPLATE);
+//			System.out.println(html);
+		} catch (FormulaAnalysisException e) {
+			e.printStackTrace();
+		}
+	}
+}
