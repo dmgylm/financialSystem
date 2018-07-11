ab781fc03902063cb14374bae620f3d3a72c6b18
chengsen <chengsen@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-07-11 06:22:26 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@60688 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/test/java/cn/springmvc/test/OrganizationMoveTest.java
+++ src/test/java/cn/springmvc/test/OrganizationMoveTest.java
@@ -9,11 +9,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import cn.financial.model.OrganizationMove;
 import cn.financial.service.impl.OrganizationMoveServiceImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 @ContextConfiguration(locations = { "classpath:spring/spring.xml", "classpath:spring/spring-mvc.xml",
         "classpath:spring/spring-mybatis.xml", "classpath:spring/mybatis-config.xml",
         "classpath:spring/spring-cache.xml", "classpath:spring/spring-shiro.xml", "classpath:spring/spring-redis.xml" })
@@ -26,9 +28,9 @@ public class OrganizationMoveTest {
 	@Test
 	public void listOrganizationMoveBy() {
 		Map<Object,Object> map = new HashMap<>();
-		map.put("orgkey", "d59aa659dba14120acf151418f48bc14");
+		//map.put("orgkey", "49e6c77c3b1d4072973b2d92e1a687f1");
         List<OrganizationMove> list = service.listOrganizationMoveBy(map);
-        System.out.println(list.get(0).toString());
+        System.out.println(list);
 	}
 	
 }
