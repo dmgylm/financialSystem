344440c875492e7e7d9a162d7943a27564c12628
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-07-12 10:17:13 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@60826 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/model/User.java
+++ src/main/java/cn/financial/model/User.java
@@ -2,30 +2,42 @@ package cn.financial.model;
 
 import java.io.Serializable;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+
+@ApiModel(value="user对象",description="用户对象user")
 public class User implements Serializable{
     
     private static final long serialVersionUID = 1L; 
     
     public static final String INITIALCIPHER = "Welcome1";//初始密码
+    
+    /*密码校验规则：
+     * 由6-15位字符组成，组成内容必须包含（但不仅限于）：
+     * 至少6个字符（最多15个字符）、大写与小写字母、至少一个数字，支持特殊符号，但不支持空格
+     * */
+    public static final String REGEX = "(?!(^[A-Za-z]*$))(?!(^[0-9]*$))(?=(^.*[\\d].*$))(?=(^.*[a-z].*$))(?=(^.*[A-Z].*$))(?!(^.*[\\s].*$))^[0-9A-Za-z\\x21-\\x7e]{6,15}$";
 
+    @ApiModelProperty(value="用户id",name="id")
     private String id; // 用户表id
-
+    @ApiModelProperty(value="用户名",name="name")
     private String name; // 用户名
-    
+    @ApiModelProperty(value="用户名",name="realName")
     private String realName;//真实姓名
-
+    @ApiModelProperty(value="密码",name="pwd")
     private String pwd; // 密码
-    
+    @ApiModelProperty(value="工号",name="jobNumber")
     private String jobNumber;//工号   
-    
+    @ApiModelProperty(value="状态0表示离职1表示在职",name="status")
     private Integer status;//状态 
-
+    @ApiModelProperty(value="创建时间",name="createTime")
     private String createTime; // 创建时间
-
+    @ApiModelProperty(value="更新时间",name="updateTime")
     private String updateTime; // 更新时间
-    
+    @ApiModelProperty(value="密码到期时间",name="expreTime")
     private String expreTime;//密码到期时间
-
+    @ApiModelProperty(value="uuid随机数生成盐",name="salt")
     private String salt;  //uuid随机数生成
     
     public User() {
@@ -111,5 +123,5 @@ public class User implements Serializable{
 	public void setSalt(String salt) {
 		this.salt = salt;
 	}
-	
+
 }
