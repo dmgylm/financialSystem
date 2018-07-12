c5546a43195598e9ab69a6c00346febac4f002c6
zhouchunping <zhouchunping@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-07-12 05:30:44 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@60763 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/interceptor/RequestLogInterceptor.java
+++ src/main/java/cn/financial/interceptor/RequestLogInterceptor.java
@@ -14,28 +14,38 @@ public class RequestLogInterceptor implements HandlerInterceptor {
 	
 		private Logger logger = LoggerFactory.getLogger(RequestLogInterceptor.class);
 
-	    @Override
+	    @SuppressWarnings("unchecked")
+		@Override
 	    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 	        StringBuffer url = request.getRequestURL();
-	    	@SuppressWarnings("unchecked")
-			Enumeration<String> names = request.getParameterNames();
-	    	StringBuffer params = new StringBuffer("?");
-	    	while(names.hasMoreElements()){
-	    		String name = names.nextElement();
-	    		String value = request.getParameter(name);
-	    		params.append(name).append("=").append(value).append("&");
-	    	}
-	    	logger.info("前端请求URL: " + url);
-	    	logger.info("前端GET/POST请求参数: " + params.toString());
-	    	String streamString = getInputStream(request);
-	    	logger.info("前端InputStream请求参数: " + streamString);
+	        logger.info("前端请求URL: " + url);
+			Enumeration<String> paramNames = request.getParameterNames();
+	    	Enumeration<String> headerNames = request.getHeaderNames();
 	    	
+	    	StringBuffer params = joinRequestParams(request,paramNames);
+	    	StringBuffer headerParams = joinRequestParams(request,headerNames);
+	    	String streamString = getInputStream(request);
+	    	
+	    	logger.info("前端GET/POST请求参数: " + params.toString());
+	    	logger.info("前端Header请求参数: " + headerParams.toString());
+	    	logger.info("前端InputStream请求参数: " + streamString);
 	    	
 	    	addCors(response);
 			return true;
 	    }
 	    
-	    private String getInputStream(HttpServletRequest request) {
+	    private StringBuffer joinRequestParams(HttpServletRequest request,
+				Enumeration<String> names) {
+	    	StringBuffer params = new StringBuffer();
+	    	while(names.hasMoreElements()){
+	    		String name = names.nextElement();
+	    		String value = request.getParameter(name);
+	    		params.append(name).append("=").append(value).append("&");
+	    	}
+	    	return params;
+		}
+
+		private String getInputStream(HttpServletRequest request) {
 			try {
 				ServletInputStream is = request.getInputStream();
 				StringBuilder content = new StringBuilder();
