aff902c109842b802fcb3e2d3b99f9f6ac57ba7d
chengsen <chengsen@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-07-12 09:41:50 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@60819 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/model/Message.java
+++ src/main/java/cn/financial/model/Message.java
@@ -4,39 +4,54 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * 消息实体类
  * 
  * @author zlf 2018/03/13
  *
  */
+@ApiModel(value="Message对象",description="消息中心对象")
 public class Message {
 	
 	public static final Integer PAGESIZE = 100000000;
 
-    String id; // 消息id(uoId)
+	@ApiModelProperty(value="消息id",name="id",example="")
+	String id; // 消息id(uoId)
 
-    Integer status; // 消息状态(0未读；1已读)
+	@ApiModelProperty(value="消息状态(0未读;1已读)",name="status",example="")
+	Integer status; // 消息状态(0未读；1已读)
 
-    Integer theme; // 消息主题（1系统提醒）
+	@ApiModelProperty(value="消息主题(1系统提醒)",name="theme",example="")
+	Integer theme; // 消息主题(1系统提醒)
 
-    String content; // 消息内容
+	@ApiModelProperty(value="消息内容",name="content",example="")
+	String content; // 消息内容
 
-    String oId; // 组织接收信息
+	@ApiModelProperty(value="组织接收信息",name="oId",example="")
+	String oId; // 组织接收信息
     
-    String sName;//消息来源(这里指向发送用户的name)
+	@ApiModelProperty(value="消息来源(这里指向发送用户的name)",name="sName",example="")
+	String sName;//消息来源(这里指向发送用户的name)
 
-    Integer isTag; // 是否标注（0未标注；1标注）
+	@ApiModelProperty(value="是否标注(0未标注;1标注)",name="isTag",example="")
+	Integer isTag; // 是否标注(0未标注;1标注)
     
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+	@ApiModelProperty(value="创建时间",name="createTime",example="")
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     Date createTime; // 创建时间
     
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+	@ApiModelProperty(value="修改时间",name="updateTime",example="")
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     Date updateTime; // 修改时间
     
-    String uId;//用户接收信息
+	@ApiModelProperty(value="用户接收信息",name="uId",example="")
+	String uId;//用户接收信息
  
-    String fileurl;//文件路径
+	@ApiModelProperty(value="保存文件路径",name="fileurl",example="")
+	String fileurl;//保存文件路径
     
     public String getId() {
         return id;
