2b300f854f450190a310a33dace0bd734bbdb367
zhouchunping <zhouchunping@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-07-19 09:57:04 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@61344 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/interceptor/RequestLogInterceptor.java
+++ src/main/java/cn/financial/interceptor/RequestLogInterceptor.java
@@ -73,7 +73,7 @@ public class RequestLogInterceptor implements HandlerInterceptor {
 			response.setHeader("Access-Control-Allow-Origin", "*");
 	        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 	        response.setHeader("Access-Control-Max-Age", "3600");
-	        response.setHeader("Access-Control-Allow-Headers"," Origin, X-Requested-With, Content-Type, Accept, token, JSESSIONID");
+	        response.setHeader("Access-Control-Allow-Headers"," Origin, X-Requested-With, Content-Type, Accept,Connection, token, JSESSIONID");
 		}
 
 
