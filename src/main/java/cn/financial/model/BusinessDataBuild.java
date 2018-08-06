6147a7638a57729ca203bc1ce6d599cc82e52079
chengsen <chengsen@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-08-06 02:49:57 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@62407 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/model/BusinessDataBuild.java
+++ src/main/java/cn/financial/model/BusinessDataBuild.java
@@ -4,15 +4,26 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value="BusinessDataBuild对象",description="损益表生成状态记录对象")
 public class BusinessDataBuild {
 	
+	@ApiModelProperty(value="部门id",name="id",example="")
 	private String id;//部门id
 	
+	@ApiModelProperty(value="生成状态",name="status",example="")
 	private Integer status;//生成状态(0:未生成,1:已生成)
 	
+	@ApiModelProperty(value="板块",name="orgPlate",example="")
+	private String orgPlate;//板块
+	
+	@ApiModelProperty(value="创建时间",name="createTime",example="")
 	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     Date createTime; // 创建时间
 	
+	@ApiModelProperty(value="修改时间",name="updateTime",example="")
 	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     Date updateTime; // 修改时间
 
@@ -46,6 +57,14 @@ public class BusinessDataBuild {
 
 	public void setUpdateTime(Date updateTime) {
 		this.updateTime = updateTime;
+	}
+
+	public String getOrgPlate() {
+		return orgPlate;
+	}
+
+	public void setOrgPlate(String orgPlate) {
+		this.orgPlate = orgPlate;
 	}
 
 }
