af78ff01ed0b4d2f7efdfcef992ea36eee8690a9
chengsen <chengsen@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-08-06 08:17:50 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@62444 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/quartz/AccountQuartzListener.java
+++ src/main/java/cn/financial/quartz/AccountQuartzListener.java
@@ -35,7 +35,7 @@ public class AccountQuartzListener implements ServletContextListener {
 								QuartzManager.addJob(QuartzManager.getsched(), "损益表生成消息提醒任务定时器", QuartzJob.class, "* * * 11 * ?",null);//每个月11号生成损益表
 								QuartzManager.addJob(QuartzManager.getsched(), "系统停止录入数据截止日期提醒任务定时器", QuartzStatement.class, "0 0 0 5 * ?",null);//每个月5号生成系统关账提醒
 						        QuartzManager.addJob(QuartzManager.getsched(), "密码有效时间到期提醒任务定时器", QuartzPwdTime.class, "0 0 9 * * ?" , null );
-						        QuartzManager.addJob(QuartzManager.getsched(), "损益生成记录表任务定时器", QuartzBusinessData.class, "30 40 * * * ?" , null );
+						        QuartzManager.addJob(QuartzManager.getsched(), "损益生成记录表任务定时器", QuartzBusinessData.class, "30 15 * * * ?" , null );
 						} catch (SchedulerException e) {
 							e.printStackTrace();
 						}
