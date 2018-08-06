9eefc65ff29c3ab14b15c68b21c2be7900ec7e13
chengsen <chengsen@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-08-06 01:07:17 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@62386 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

---
+++ src/main/java/cn/financial/quartz/QuartzBusinessData.java
@@ -0,0 +1,63 @@
+package cn.financial.quartz;
+
+import java.util.List;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cn.financial.model.BusinessDataBuild;
+import cn.financial.model.Organization;
+import cn.financial.service.impl.BusinessDataBuildServiceImpl;
+import cn.financial.service.impl.OrganizationServiceImpl;
+
+public class QuartzBusinessData implements Job{
+	
+	private OrganizationServiceImpl oImpl;
+	
+	private BusinessDataBuildServiceImpl buildServiceImpl;
+	
+	protected Logger logger = LoggerFactory.getLogger(QuartzBusinessData.class);
+	
+	@Override
+	public void execute(JobExecutionContext arg0) throws JobExecutionException{
+		
+		long start = System.currentTimeMillis();
+		
+		buildServiceImpl = (BusinessDataBuildServiceImpl) AccountQuartzListener.getSpringContext().getBean("BusinessDataBuildServiceImpl");
+		oImpl = (OrganizationServiceImpl) AccountQuartzListener.getSpringContext().getBean("OrganizationServiceImpl");
+		
+		System.out.println("启动损益状态生成记录任务~~~~");
+		
+		final Integer THREE = 3;
+		
+		buildServiceImpl.deleteBusinessDataBuild();
+		
+		System.out.println("清空表完成~~~~");
+		
+		List<Organization> list = oImpl.listOrganizationBy(null, null, null, null, null, null, null, null, null);
+		
+		for(int i=0; i<list.size(); i++) {
+			
+			int num = list.get(i).getOrgType();
+			if(num == THREE) {
+				BusinessDataBuild bdb = new BusinessDataBuild();
+				bdb.setId(list.get(i).getId());
+				bdb.setStatus(0);
+				buildServiceImpl.saveBusinessDataBuild(bdb);
+			}
+			
+		}
+		
+		System.out.println("新增表完成~~~~");
+		
+		long end = System.currentTimeMillis();
+		
+		System.out.println("时间花费~~~~");
+		System.out.println(end - start);
+				
+	}
+	
+}
