5c3ea2e96c0c8b3c2fa4283145ba37129d0d9e9c
chengsen <chengsen@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-08-06 01:07:33 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@62388 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

---
+++ src/main/java/cn/financial/service/impl/BusinessDataBuildServiceImpl.java
@@ -0,0 +1,39 @@
+package cn.financial.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.financial.dao.BusinessDataBuildDao;
+import cn.financial.model.BusinessDataBuild;
+import cn.financial.service.BusinessDataBuildService;
+
+@Service("BusinessDataBuildServiceImpl")
+public class BusinessDataBuildServiceImpl implements BusinessDataBuildService{
+	
+	@Autowired
+	private BusinessDataBuildDao dao;
+	
+	@Override
+	public Integer saveBusinessDataBuild(BusinessDataBuild businessDataBuild) {
+		return dao.saveBusinessDataBuild(businessDataBuild);
+	}
+	
+	@Override
+	public Integer deleteBusinessDataBuild() {
+		return dao.deleteBusinessDataBuild();
+	}
+	
+	@Override
+	public Integer updateBusinessDataBuild(Map<Object, Object> map) {
+		return dao.updateBusinessDataBuild(map);
+	}
+	
+	@Override
+	public List<BusinessDataBuild> listBusinessDataBuild(Map<Object, Object> map){
+		return dao.listBusinessDataBuild(map);
+	}
+
+}
