6aa28d0d1dab6f15da8de7ee011ce48d5fb9d8df
chengsen <chengsen@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-08-08 03:09:27 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@62532 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/util/SiteConst.java
+++ src/main/java/cn/financial/util/SiteConst.java
@@ -15,4 +15,6 @@ public abstract interface SiteConst {
 	public static final String  CAPITALEXPORT=PropertiesUtils.getProperty("CAPITALEXPORT");
 	//损益表生成日(如:11),用于判断是否可以编辑当月预算
 	public static final int PROFIT_LOSS_GENERATE_DAY = Integer.parseInt(PropertiesUtils.getProperty("PROFIT_LOSS_GENERATE_DAY"));
+	//损益表生成定时器日期
+	public static final String DAY_TIME = PropertiesUtils.getProperty("DAY_TIME");
 }
