2fd53b232834892140652b87ac9f8780df1f31d3
hushulin <hushulin@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-08-08 04:14:22 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@62541 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/util/SiteConst.java
+++ src/main/java/cn/financial/util/SiteConst.java
@@ -17,4 +17,8 @@ public abstract interface SiteConst {
 	public static final int PROFIT_LOSS_GENERATE_DAY = Integer.parseInt(PropertiesUtils.getProperty("PROFIT_LOSS_GENERATE_DAY"));
 	//损益表生成定时器日期
 	public static final String DAY_TIME = PropertiesUtils.getProperty("DAY_TIME");
+	//开始时间
+	public static final int MOVE_ORGANIZATION_START_TIME=Integer.parseInt(PropertiesUtils.getProperty("MOVE_ORGANIZATION_START_TIME"));
+	//结束时间
+	public static final int MOVE_ORGANIZATION_STOP_TIME=Integer.parseInt(PropertiesUtils.getProperty("MOVE_ORGANIZATION_STOP_TIME"));
 }
