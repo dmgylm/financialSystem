86d6e3cac52d9f0569321da596c58b1edb161253
ranzhenming <ranzhenming@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-08-22 09:54:33 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@63479 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/service/impl/BusinessDataInfoServiceImpl.java
+++ src/main/java/cn/financial/service/impl/BusinessDataInfoServiceImpl.java
@@ -20,13 +20,15 @@ import cn.financial.util.HtmlGenerate;
 
 /**
  * 损益表Service
+ * 
  * @author lmn
  *
  */
 @Service("BusinessDataInfoServiceImpl")
-public class BusinessDataInfoServiceImpl implements BusinessDataInfoService{
+public class BusinessDataInfoServiceImpl implements BusinessDataInfoService {
 	@Autowired
 	private BusinessDataInfoDao businessDataInfoDao;
+
 	@Override
 	public Integer insertBusinessDataInfo(BusinessDataInfo businessData) {
 		return businessDataInfoDao.insertBusinessDataInfo(businessData);
@@ -51,83 +53,92 @@ public class BusinessDataInfoServiceImpl implements BusinessDataInfoService{
 	public BusinessDataInfo selectBusinessDataById(String id) {
 		return businessDataInfoDao.selectBusinessDataById(id);
 	}
-	
+
 	/**
 	 * 
-	 * @param dataMjo 原始模板
-	 * @param mo   前端传来的HTML
+	 * @param dataMjo
+	 *            原始模板
+	 * @param mo
+	 *            前端传来的HTML
 	 * @return 返回合并后的模板
 	 */
-	public JSONObject dgkey(JSONObject dataMjo, Map<String,Object> mo,String type) {
-		JSONObject newBudgetHtml=new JSONObject();
-		JSONObject ja=new JSONObject();
-		if(type.equals("BUDGET")) {//预算
+	public JSONObject dgkey(JSONObject dataMjo, Map<String, Object> mo, String type) {
+		JSONObject newBudgetHtml = new JSONObject();
+		JSONObject ja = new JSONObject();
+		if (type.equals("BUDGET")) {// 预算
 			for (Iterator<String> iterIncome = dataMjo.keySet().iterator(); iterIncome.hasNext();) {// 获得损益模板最外层key
 				String keyIncome = iterIncome.next();
 				Object objIncome = dataMjo.get(keyIncome);
-				if (objIncome instanceof JSONArray) {
-					newBudgetHtml=mergeHtml(dataMjo, mo);
+				if (keyIncome.equals("title")) {
+					ja.put(keyIncome, dataMjo.get(keyIncome));
 				} else {
-					JSONObject longData = (JSONObject) objIncome;
-					newBudgetHtml=mergeHtml(longData, mo);
+					if (objIncome instanceof JSONArray) {
+						newBudgetHtml = mergeHtml(dataMjo, mo);
+					} else {
+						JSONObject longData = (JSONObject) objIncome;
+						newBudgetHtml = mergeHtml(longData, mo);
+					}
+					ja.put(keyIncome, newBudgetHtml);
 				}
-				ja.put(keyIncome, newBudgetHtml);
 			}
 			return ja;
-		}else if(type.equals("PROFIT_LOSS")) {//损益
+		} else if (type.equals("PROFIT_LOSS")) {// 损益
 			for (Iterator<String> iterIncome = dataMjo.keySet().iterator(); iterIncome.hasNext();) {// 获得损益模板最外层key
 				String keyIncome = iterIncome.next();
 				Object objIncome = dataMjo.get(keyIncome);
 				if (objIncome instanceof JSONArray) {
-					newBudgetHtml=mergeHtml(dataMjo, mo);
+					newBudgetHtml = mergeHtml(dataMjo, mo);
 				} else {
 					JSONObject longData = (JSONObject) objIncome;
-					newBudgetHtml=mergeHtml(longData, mo);
+					newBudgetHtml = mergeHtml(longData, mo);
 				}
 			}
 			return newBudgetHtml;
 		}
-	
+
 		return null;
 	}
+
 	/**
 	 * 根据前端传来的表格进行公式计算之后进行合并
+	 * 
 	 * @param dataMjo
 	 * @param mo
 	 * @return
 	 */
-	public JSONObject mergeHtml (JSONObject dataMjo,Map<String,Object> mo) {
-		JSONObject newBudgetHtml=new JSONObject();
+	public JSONObject mergeHtml(JSONObject dataMjo, Map<String, Object> mo) {
+		JSONObject newBudgetHtml = new JSONObject();
 		JSONObject obj = new JSONObject();
 		for (Iterator<String> iterIncome = dataMjo.keySet().iterator(); iterIncome.hasNext();) {// 获得损益模板最外层key
 			String keyIncome = iterIncome.next();
 			Object objIncome = dataMjo.get(keyIncome);
 			JSONArray jsoA = new JSONArray();
 			if (objIncome instanceof JSONArray) {
-			JSONArray longLst = (JSONArray) objIncome;
+				JSONArray longLst = (JSONArray) objIncome;
 				for (int j = 0; j < longLst.size(); j++) {
-					 obj = new JSONObject();
+					obj = new JSONObject();
 					JSONObject longJson = longLst.getJSONObject(j);
 					for (Iterator<String> iters = longJson.keySet().iterator(); iters.hasNext();) {
 						String keysIncomes = iters.next();
 						Object objsIncome = longJson.get(keysIncomes);
 						obj.put(keysIncomes, objsIncome);
-						
-						if(mo.containsKey(longJson.get("key"))) {
-							double value= 0.0;
-							if(Integer.parseInt(longJson.get("type").toString())==HtmlGenerate.BOX_TYPE_FORMULA) {//判断当前类型是不是公式
-								 value=FormulaUtil.calculationByFormula(mo,longJson.get("reallyFormula").toString());
-							}else {
-								value=Double.parseDouble((mo.get(longJson.get("key")).toString()).toString());
+
+						if (mo.containsKey(longJson.get("key"))) {
+							double value = 0.0;
+							if (Integer.parseInt(longJson.get("type").toString()) == HtmlGenerate.BOX_TYPE_FORMULA) {// 判断当前类型是不是公式
+								value = FormulaUtil.calculationByFormula(mo, longJson.get("reallyFormula").toString());
+							} else {
+								value = Double.parseDouble((mo.get(longJson.get("key")).toString()).toString());
 							}
 							obj.put("value", value);
 						}
 					}
-					jsoA.add(obj);;
+					jsoA.add(obj);
+					;
 				}
 			}
-		newBudgetHtml.put(keyIncome, jsoA);
+			newBudgetHtml.put(keyIncome, jsoA);
 		}
-		return newBudgetHtml ;
+		return newBudgetHtml;
 	}
 }
