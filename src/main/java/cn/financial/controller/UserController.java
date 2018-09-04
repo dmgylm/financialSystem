23f9b8adcccf44c5d6f511fca5a79cced5c94d86
guosai <guosai@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-09-04 02:42:04 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@64350 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/controller/UserController.java
+++ src/main/java/cn/financial/controller/UserController.java
@@ -219,7 +219,7 @@ public class UserController{
         @ApiResponse(code = 208, message = "密码由6～15位数字、大小写字母组成"),@ApiResponse(code = 217, message = "用户组织架构关联信息不存在"),
         @ApiResponse(code = 218, message = "用户角色关联系信息不存在")})
     @ResponseBody
-    public ResultUtils update(String userId, String name, String realName, Integer status, String jobNumber,String roleId,String orgId) {
+    public ResultUtils update(HttpServletRequest request, String userId, String name, String realName, Integer status, String jobNumber,String roleId,String orgId) {
     	ResultUtils result = new ResultUtils();
     	
     	try {
@@ -260,6 +260,49 @@ public class UserController{
             //修改用户组织架构关联信息，先删除用户关联的组织架构信息，再重新添加该用户的组织架构信息
             Integer userOrganizationList = 0;
             if(orgId != null && !"".equals(orgId)) {
+                User currentUser = (User) request.getAttribute("user");
+                List<UserOrganization> userOrgPermission = new ArrayList<>();
+                List<Organization> listOrg = new ArrayList<>();
+                if(currentUser.getId()!=null && !currentUser.getId().equals("")){
+                    //根据当前登录用户id查询组织节点信息
+                    userOrgPermission.addAll(userOrganizationService.listUserOrganization(currentUser.getId()));
+                }
+                if(!CollectionUtils.isEmpty(userOrgPermission)){
+                    for(UserOrganization orgCode : userOrgPermission){
+                        //根据code查询组织架构信息(查询当前登录人orgId信息)
+                        listOrg.addAll(organizationService.listOrganizationBy(null,null,null,null,orgCode.getCode(),null,null,null,null));
+                    }
+                }
+                //查询需要修改的用户orgId信息
+                List<UserOrganization> userOrgPermissions = new ArrayList<>();
+                List<Organization> listOrgs = new ArrayList<>();
+                userOrgPermissions.addAll(userOrganizationService.listUserOrganization(userId));
+                if(!CollectionUtils.isEmpty(userOrgPermissions)){
+                    for(UserOrganization orgCode : userOrgPermissions){
+                        //根据code查询组织架构信息(查询需要修改的用户orgId信息)
+                        listOrgs.addAll(organizationService.listOrganizationBy(null,null,null,null,orgCode.getCode(),null,null,null,null));
+                    }
+                }
+                //当前登录人的orgId
+                List<String> org = new ArrayList<>();//需要修改的orgId
+                if(!CollectionUtils.isEmpty(listOrg)){
+                    for(Organization orgCode : listOrg){
+                        org.add(orgCode.getId());
+                    }
+                }
+                //需要修改的orgId
+                List<String> orgs = new ArrayList<>();//需要修改的orgId
+                if(!CollectionUtils.isEmpty(listOrgs)){
+                    for(Organization orgCode : listOrgs){
+                        orgs.add(orgCode.getId());
+                    }
+                }
+                //循环检索需要修改的orgId是否包括在当前登录人的orgId之内
+                if(!org.containsAll(orgs)){
+                    ElementXMLUtils.returnValue(ElementConfig.USER_ORGANIZATION_RESULT, result);
+                    TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+                    return result;
+                } 
             	UserOrganization userOrganization = null;
                 userOrganization = new UserOrganization();
                 userOrganization.setId(UuidUtil.getUUID());
