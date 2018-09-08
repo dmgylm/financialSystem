84045e0ef2ec6f6f28122206033ff751878b6e50
gonghao <gonghao@ced6f03c-c1bb-49ab-ae0c-4da96410de7b>
2018-09-08 08:10:36 +0000

    git-svn-id: svn://192.168.2.130:8807/sd/java/financialSystem@64774 ced6f03c-c1bb-49ab-ae0c-4da96410de7b

--- src/main/java/cn/financial/service/impl/StatisticJsonServiceImpl.java
+++ src/main/java/cn/financial/service/impl/StatisticJsonServiceImpl.java
@@ -557,14 +557,14 @@ public class StatisticJsonServiceImpl implements StatisticJsonService {
 							String downKey = dt.next();
 							Double valve = downValve.getDouble(downKey);
 							if(item.containsKey(downKey)){
-								valve +=Double.valueOf(item.get(downKey).toString());
+								valve +=Double.parseDouble(item.get(downKey).toString());
 							}
 							item.put(downKey,df.format(valve));
 						}
 					}else{
 						Double valve = jsonValve.getDouble(itemKey);
 						if(item.containsKey(itemKey)){
-							valve +=Double.valueOf(item.get(itemKey).toString());
+							valve +=Double.parseDouble(item.get(itemKey).toString());
 						}
 						item.put(itemKey, df.format(valve));
 					}
